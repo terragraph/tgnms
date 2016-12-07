8aa25a69f4c285de86ed58aae09d1dfb655a7592
Tariq Al-Khasib <talkhasib@fb.com>
2016-12-06 17:01:29 -0800

    stub out controller zmq interactions

---
+++ controllerProxy.js
@@ -0,0 +1,118 @@
+const zmq = require('zmq');
+// thrift serialization
+const thrift = require('thrift');
+
+var Topology_ttypes = require('./thrift/gen-nodejs/Topology_types');
+var Controller_ttypes = require('./thrift/gen-nodejs/Controller_types');
+
+var self = {
+  getTopology: function (index, configs, receivedTopologies) {
+    let config = configs[index];
+    // guard against hanging
+    var timeout = setTimeout(function(){
+      receivedTopologies[index] = {};
+      dealer.close();
+    }, 1000);
+
+    let dealer = zmq.socket('dealer');
+    dealer.identity = 'NMS_WEB_TOPO';
+    dealer.setsockopt(zmq.ZMQ_IPV4ONLY, 0);
+    dealer.setsockopt(zmq.ZMQ_LINGER, 0);
+    dealer.connect('tcp://[' + config.controller_ip +']:17077');
+    dealer.on('message', function (receiver, senderApp, msg) {
+      clearTimeout(timeout);
+      // Deserialize Message to get mType
+      var tTransport = new thrift.TFramedTransport(msg);
+      var tProtocol = new thrift.TCompactProtocol(tTransport);
+      var receivedMessage = new Controller_ttypes.Message();
+      receivedMessage.read(tProtocol);
+
+      // Deserialize body
+      msg = msg.slice(6);
+      tTransport = new thrift.TFramedTransport(msg);
+      tProtocol = new thrift.TCompactProtocol(tTransport);
+      var receivedTopology = new Topology_ttypes.Topology();
+      receivedTopology.read(tProtocol);
+      receivedTopologies[index] = receivedTopology;
+      dealer.close();
+    });
+
+    dealer.on('error', function(err) {
+      clearTimeout(timeout);
+      console.error(err);
+      dealer.close();
+    });
+
+    var transport = new thrift.TFramedTransport(null, function(byteArray) {
+      // Flush puts a 4-byte header, which needs to be parsed/sliced.
+       byteArray = byteArray.slice(4);
+       dealer.send("", zmq.ZMQ_SNDMORE);
+       dealer.send("ctrl-app-TOPOLOGY_APP", zmq.ZMQ_SNDMORE);
+       dealer.send("NMS_WEB_TOPO", zmq.ZMQ_SNDMORE);
+       dealer.send(byteArray);
+    });
+
+    var tProtocol = new thrift.TCompactProtocol(transport);
+    var topologyReqMessage = new Controller_ttypes.Message();
+    topologyReqMessage.mType = Controller_ttypes.MessageType.GET_TOPOLOGY;
+    topologyReqMessage.value = '\0';
+    topologyReqMessage.write(tProtocol);
+    transport.flush();
+  },
+
+  getStatusDump: function (index, configs, statusDumps) {
+    let config = configs[index];
+    // guard against hanging
+    var timeout = setTimeout(function(){
+      statusDumps[index] = {};
+      dealer.close();
+    }, 2000);
+
+    let dealer = zmq.socket('dealer');
+    dealer.identity = 'NMS_WEB_STATUS';
+    dealer.setsockopt(zmq.ZMQ_IPV4ONLY, 0);
+    dealer.setsockopt(zmq.ZMQ_LINGER, 0);
+    dealer.connect('tcp://[' + config.controller_ip +']:17077');
+    dealer.on('message', function (receiver, senderApp, msg) {
+      clearTimeout(timeout);
+      // Deserialize Message to get mType
+      var tTransport = new thrift.TFramedTransport(msg);
+      var tProtocol = new thrift.TCompactProtocol(tTransport);
+      var receivedMessage = new Controller_ttypes.Message();
+      receivedMessage.read(tProtocol);
+
+      // Deserialize body
+      msg = msg.slice(6);
+      tTransport = new thrift.TFramedTransport(msg);
+      tProtocol = new thrift.TCompactProtocol(tTransport);
+      var statusDump = new Controller_ttypes.StatusDump();
+      statusDump.read(tProtocol);
+      statusDumps[index] = statusDump;
+      dealer.close();
+    });
+
+    dealer.on('error', function(err) {
+      clearTimeout(timeout);
+      console.error(err);
+      dealer.close();
+    });
+
+    var transport = new thrift.TFramedTransport(null, function(byteArray) {
+      // Flush puts a 4-byte header, which needs to be parsed/sliced.
+       byteArray = byteArray.slice(4);
+       dealer.send("", zmq.ZMQ_SNDMORE);
+       dealer.send("ctrl-app-STATUS_APP", zmq.ZMQ_SNDMORE);
+       dealer.send("NMS_WEB_STATUS", zmq.ZMQ_SNDMORE);
+       dealer.send(byteArray);
+    });
+
+    var tProtocol = new thrift.TCompactProtocol(transport);
+    var statusDumpMessage = new Controller_ttypes.Message();
+    statusDumpMessage.mType = Controller_ttypes.MessageType.GET_STATUS_DUMP;
+    statusDumpMessage.value = '\0';
+    statusDumpMessage.write(tProtocol);
+    transport.flush();
+  }
+}
+
+module.exports = self;
--- server.js
+++ server.js
@@ -10,13 +10,8 @@ const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping && process.env.PORT ? process.env.PORT : 8080;
 const app = express();
 const fs = require('fs');
-const zmq = require('zmq');
 const influxHelper = require('./influx');
-
-// thrift serialization
-const thrift = require('thrift');
-var Topology_ttypes = require('./thrift/gen-nodejs/Topology_types');
-var Controller_ttypes = require('./thrift/gen-nodejs/Controller_types');
+const controllerProxy = require('./controllerProxy');
 
 var fileTopologies = [];
 var configs = [];
@@ -24,118 +19,10 @@ var topologies_index = 0;
 var receivedTopologies = [];
 var statusDumps = [];
 
-function getTopology(index) {
-  let config = configs[index];
-  // guard against hanging
-  var timeout = setTimeout(function(){
-    receivedTopologies[index] = {};
-    dealer.close();
-  }, 1000);
-
-  let dealer = zmq.socket('dealer');
-  dealer.identity = 'NMS_WEB_TOPO';
-  dealer.setsockopt(zmq.ZMQ_IPV4ONLY, 0);
-  dealer.setsockopt(zmq.ZMQ_LINGER, 0);
-  dealer.connect('tcp://[' + config.controller_ip +']:17077');
-  dealer.on('message', function (receiver, senderApp, msg) {
-    clearTimeout(timeout);
-    // Deserialize Message to get mType
-    var tTransport = new thrift.TFramedTransport(msg);
-    var tProtocol = new thrift.TCompactProtocol(tTransport);
-    var receivedMessage = new Controller_ttypes.Message();
-    receivedMessage.read(tProtocol);
-
-    // Deserialize body
-    msg = msg.slice(6);
-    tTransport = new thrift.TFramedTransport(msg);
-    tProtocol = new thrift.TCompactProtocol(tTransport);
-    var receivedTopology = new Topology_ttypes.Topology();
-    receivedTopology.read(tProtocol);
-    receivedTopologies[index] = receivedTopology;
-    dealer.close();
-  });
-
-  dealer.on('error', function(err) {
-    clearTimeout(timeout);
-    console.error(err);
-    dealer.close();
-  });
-
-  var transport = new thrift.TFramedTransport(null, function(byteArray) {
-    // Flush puts a 4-byte header, which needs to be parsed/sliced.
-     byteArray = byteArray.slice(4);
-     dealer.send("", zmq.ZMQ_SNDMORE);
-     dealer.send("ctrl-app-TOPOLOGY_APP", zmq.ZMQ_SNDMORE);
-     dealer.send("NMS_WEB_TOPO", zmq.ZMQ_SNDMORE);
-     dealer.send(byteArray);
-  });
-
-  var tProtocol = new thrift.TCompactProtocol(transport);
-  var topologyReqMessage = new Controller_ttypes.Message();
-  topologyReqMessage.mType = Controller_ttypes.MessageType.GET_TOPOLOGY;
-  topologyReqMessage.value = '\0';
-  topologyReqMessage.write(tProtocol);
-  transport.flush();
-}
-
-function getStatusDump(index) {
-  let config = configs[index];
-  // guard against hanging
-  var timeout = setTimeout(function(){
-    statusDumps[index] = {};
-    dealer.close();
-  }, 2000);
-
-  let dealer = zmq.socket('dealer');
-  dealer.identity = 'NMS_WEB_STATUS';
-  dealer.setsockopt(zmq.ZMQ_IPV4ONLY, 0);
-  dealer.setsockopt(zmq.ZMQ_LINGER, 0);
-  dealer.connect('tcp://[' + config.controller_ip +']:17077');
-  dealer.on('message', function (receiver, senderApp, msg) {
-    clearTimeout(timeout);
-    // Deserialize Message to get mType
-    var tTransport = new thrift.TFramedTransport(msg);
-    var tProtocol = new thrift.TCompactProtocol(tTransport);
-    var receivedMessage = new Controller_ttypes.Message();
-    receivedMessage.read(tProtocol);
-
-    // Deserialize body
-    msg = msg.slice(6);
-    tTransport = new thrift.TFramedTransport(msg);
-    tProtocol = new thrift.TCompactProtocol(tTransport);
-    var statusDump = new Controller_ttypes.StatusDump();
-    statusDump.read(tProtocol);
-    statusDumps[index] = statusDump;
-    dealer.close();
-  });
-
-  dealer.on('error', function(err) {
-    clearTimeout(timeout);
-    console.error(err);
-    dealer.close();
-  });
-
-  var transport = new thrift.TFramedTransport(null, function(byteArray) {
-    // Flush puts a 4-byte header, which needs to be parsed/sliced.
-     byteArray = byteArray.slice(4);
-     dealer.send("", zmq.ZMQ_SNDMORE);
-     dealer.send("ctrl-app-STATUS_APP", zmq.ZMQ_SNDMORE);
-     dealer.send("NMS_WEB_STATUS", zmq.ZMQ_SNDMORE);
-     dealer.send(byteArray);
-  });
-
-  var tProtocol = new thrift.TCompactProtocol(transport);
-  var statusDumpMessage = new Controller_ttypes.Message();
-  statusDumpMessage.mType = Controller_ttypes.MessageType.GET_STATUS_DUMP;
-  statusDumpMessage.value = '\0';
-  statusDumpMessage.write(tProtocol);
-  transport.flush();
-}
-
 function periodicNetworkStatus() {
   for (var i = 0, len = configs.length; i < len; i++) {
-    getTopology(i);
-    getStatusDump(i);
+    controllerProxy.getTopology(i, configs, receivedTopologies);
+    controllerProxy.getStatusDump(i, configs, statusDumps);
   }
 }
 
@@ -153,7 +40,6 @@ if (isDeveloping) {
       modules: false
     }
   });
-
 
   // Read list of networks and start timer to pull network status/topology
   fs.readFile('./config/network_config.materialized_JSON', 'utf-8', (err, data) => {
@@ -188,16 +74,6 @@ if (isDeveloping) {
   // single node
   app.get(/\/influx\/([a-z_]+)\/([a-z0-9\:\,]+)$/i, function (req, res, next) {
     influxHelper.query(req, res, next);
-  });
-  // single node, multiple terms
-  app.get(/\/topology\/refresh$/, function (req, res, next) {
-    let dealer = zmq.socket('dealer');
-    dealer.connect('tcp://[2620:10d:c089:e00c:20C:29FF:FE69:6C10]:17077');
-    dealer.send('');
-    dealer.send('ctrl-app-TOPOLOGY_APP');
-    dealer.send('NMS_WEB');
-    let topologyReq = t.getTopologyReq();
-    dealer.send(topologyReq);
   });
   app.get(/\/topology\/static$/, function(req, res, next) {
     fs.readFile('./config/network_config.materialized_JSON', 'utf-8', (err, data) => {
