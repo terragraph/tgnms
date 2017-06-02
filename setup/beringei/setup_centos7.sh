b9163e4ee838df53970f142a5d6bdbb7c9f6e954
Paul McCutcheon <pmccutcheon@fb.com>
2017-06-02 11:07:56 -0700

    beringei install helper scripts

---
+++ setup/beringei/setup_centos7.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -e
+
+FB_VERSION="2017.04.24.00"
+ZSTD_VERSION="1.1.1"
+
+echo "This script configures CentOS 7 with everything needed to run beringei."
+echo "It requires that you run it as root. sudo works great for that."
+
+ready_destdir() {
+        if [[ -e ${2} ]]; then
+                echo "Moving aside existing $1 directory.."
+                mv -v "$2" "$2.bak.$(date +%Y-%m-%d)"
+        fi
+}
+
+mkdir -pv /usr/local/facebook-${FB_VERSION}
+ln -sfT /usr/local/facebook-${FB_VERSION} /usr/local/facebook
+
+export LDFLAGS="-L/usr/local/facebook/lib -Wl,-rpath=/usr/local/facebook/lib"
+export CPPFLAGS="-I/usr/local/facebook/include"
+
+cd /tmp
+
+wget -O /tmp/folly-${FB_VERSION}.tar.gz https://github.com/facebook/folly/archive/v${FB_VERSION}.tar.gz
+wget -O /tmp/wangle-${FB_VERSION}.tar.gz https://github.com/facebook/wangle/archive/v${FB_VERSION}.tar.gz
+wget -O /tmp/fbthrift-${FB_VERSION}.tar.gz https://github.com/facebook/fbthrift/archive/v${FB_VERSION}.tar.gz
+wget -O /tmp/proxygen-${FB_VERSION}.tar.gz https://github.com/facebook/proxygen/archive/v${FB_VERSION}.tar.gz
+wget -O /tmp/mstch-master.tar.gz https://github.com/no1msd/mstch/archive/master.tar.gz
+wget -O /tmp/zstd-${ZSTD_VERSION}.tar.gz https://github.com/facebook/zstd/archive/v${ZSTD_VERSION}.tar.gz
+
+tar xzvf folly-${FB_VERSION}.tar.gz
+tar xzvf wangle-${FB_VERSION}.tar.gz
+tar xzvf fbthrift-${FB_VERSION}.tar.gz
+tar xzvf proxygen-${FB_VERSION}.tar.gz
+tar xzvf mstch-master.tar.gz
+tar xzvf zstd-${ZSTD_VERSION}.tar.gz
+
+pushd mstch-master
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/facebook-${FB_VERSION} .
+make install
+popd
+
+pushd zstd-${ZSTD_VERSION}
+make install PREFIX=/usr/local/facebook-${FB_VERSION}
+popd
+
+
+pushd folly-${FB_VERSION}/folly
+autoreconf -ivf
+./configure --prefix=/usr/local/facebook-${FB_VERSION}
+make install
+popd
+
+pushd wangle-${FB_VERSION}/wangle
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/facebook-${FB_VERSION} -DBUILD_SHARED_LIBS:BOOL=ON .
+make
+# Wangle tests are broken. Disabling ctest.
+# ctest
+make install
+popd
+
+pushd fbthrift-${FB_VERSION}/thrift
+autoreconf -ivf
+./configure --prefix=/usr/local/facebook-${FB_VERSION}
+make install
+popd
+
+pushd proxygen-${FB_VERSION}/proxygen
+autoreconf -ivf
+./configure --prefix=/usr/local/facebook-${FB_VERSION}
+make install
+popd
