a2ddb72880326a0cc5536abe984642763c32b458
Paul McCutcheon <pmccutcheon@fb.com>
2017-08-11 14:52:48 -0700

    Show polarity/node type overview

--- www/app/DetailsTopology.js
+++ www/app/DetailsTopology.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Actions } from './NetworkConstants.js';
 import Dispatcher from './NetworkDispatcher.js';
-import { availabilityColor } from './NetworkHelper.js';
+import { availabilityColor, polarityColor } from './NetworkHelper.js';
 import swal from 'sweetalert';
 import 'sweetalert/dist/sweetalert.css';
 
@@ -42,9 +42,113 @@ export default class DetailsTopology extends React.Component {
       wirelessLinksCount++;
       alivePercAvg += alivePerc;
     });
+    let nodeTypes = {};
+    let polarities = {};
+    // { site name, polarity }
+    let polarityBySite = {};
+    //console.log(this.props);
+    this.props.topology.nodes.forEach(node => {
+      // calculate # of dns, cns
+      if (!nodeTypes.hasOwnProperty(node.node_type)) {
+        nodeTypes[node.node_type] = 0;
+      }
+      nodeTypes[node.node_type]++;
+      // polarities
+      if (!polarities.hasOwnProperty(node.polarity)) {
+        polarities[node.polarity] = 0;
+      }
+      polarities[node.polarity]++;
+      // polarity by site
+      if (!polarityBySite.hasOwnProperty(node.site_name)) {
+        polarityBySite[node.site_name] = node.polarity;
+      } else {
+        polarityBySite[node.site_name] =
+          (polarityBySite[node.site_name] != node.polarity) ?
+            3 /* HYBRID */ :
+            node.polarity;
+      }
+    });
     alivePercAvg /= wirelessLinksCount;
     alivePercAvg = parseInt(alivePercAvg * 1000) / 1000.0;
 
+    let nodeTypeRows = Object.keys(nodeTypes).map((nodeType, nodeIndex) => {
+      let nodeTypeName = "Unknown";
+      if (nodeType == 1) {
+        nodeTypeName = 'CN';
+      } else if (nodeType == 2) {
+        nodeTypeName = 'DN';
+      }
+      let nodeTypeCount = nodeTypes[nodeType];
+      let nodeTypeCountPerc = 
+        (parseInt(nodeTypeCount / this.props.topology.nodes.length * 100));
+      return (
+        <tr key={"nodeType-" + nodeType}>
+          {nodeIndex == 0 ?
+            <td width="150px" rowSpan={Object.keys(nodeTypes).length}>Node Types</td> : ""}
+          <td>
+            {nodeTypeName}
+          </td>
+          <td>
+            {nodeTypeCount} ({nodeTypeCountPerc}%)
+          </td>
+        </tr>
+      );
+    });
+    let polarityBySiteRows = Object.values(polarityBySite).map((polarity, index) => {
+      //console.log('polarity of site', polarity);
+      polarity = parseInt(polarity);
+      let polarityName = "Not Set";
+      if (polarity == 1) {
+        polarityName = 'Odd';
+      } else if (polarity == 2) {
+        polarityName = 'Even';
+      } else {
+        polarityName = 'Hybrid';
+      }
+      let polarityCount = polarityBySite[polarity];
+      let polarityCountPerc = 
+        (parseInt(polarityCount / this.props.topology.sites.length * 100));
+      return (
+        <tr key={"polarityBySite-" + polarity}>
+          {index == 0 ?
+            <td width="150px" rowSpan={Object.keys(polarityBySite).length}>Polarities (Site)</td> : ""}
+          <td>
+            <span style={{color: polarityColor(parseInt(polarity))}}>
+              {polarityName}
+            </span>
+          </td>
+          <td>
+            {polarityCount} ({polarityCountPerc}%)
+          </td>
+        </tr>
+      );
+    });
+    let polarityRows = Object.keys(polarities).map((polarity, index) => {
+      polarity = parseInt(polarity);
+      let polarityName = "Not Set";
+      if (polarity == 1) {
+        polarityName = 'Odd';
+      } else if (polarity == 2) {
+        polarityName = 'Even';
+      }
+      let polarityCount = polarities[polarity];
+      let polarityCountPerc = 
+        (parseInt(polarityCount / this.props.topology.nodes.length * 100));
+      return (
+        <tr key={"polarity-" + polarity}>
+          {index == 0 ?
+            <td width="150px" rowSpan={Object.keys(polarities).length}>Polarities (Sector)</td> : ""}
+          <td>
+            <span style={{color: polarityColor(polarity)}}>
+              {polarityName}
+            </span>
+          </td>
+          <td>
+            {polarityCount} ({polarityCountPerc}%)
+          </td>
+        </tr>
+      );
+    });
     return (
       <div id="myModal" className="details">
         <div className="details-content">
@@ -57,12 +161,14 @@ export default class DetailsTopology extends React.Component {
               <tbody>
                 <tr>
                   <td width="150px">Availability (24 Hours)</td>
-                  <td>
+                  <td colSpan="2">
                     <span style={{color: availabilityColor(alivePercAvg)}}>
                       {linksWithData ? alivePercAvg+'%' : 'No Data'}
                     </span>
                   </td>
                 </tr>
+                {nodeTypeRows}
+                {polarityRows}
               </tbody>
             </table>
           </div>
