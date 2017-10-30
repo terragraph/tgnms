a8bf16e72ad4fe19190d42015e6108b1f245a75d
Kelvin Jiang <kelvinjiang@fb.com>
2017-10-30 11:43:01 -0700

    convert the nodes table to a bootstrap table

--- www/app/components/networkconfig/NetworkConfigNodes.js
+++ www/app/components/networkconfig/NetworkConfigNodes.js
@@ -8,51 +8,72 @@ const classNames = require('classnames');
 
 import { CONFIG_VIEW_MODE } from '../../constants/NetworkConfigConstants.js';
 import {changeEditMode, selectNodes} from '../../actions/NetworkConfigActions.js';
-
-// export default class
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 export default class NetworkConfigNodes extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  selectNode = (node) => {
+  formatNodeName = (cell, row, enumObject, index) => {
+    const nodeName = row.name;
+    const nodesWithDraftsSet = new Set(this.props.nodesWithDrafts);
+
+    const unsavedAsterisk = nodesWithDraftsSet.has(nodeName) ? (
+      <span style={{color: '#cc0000', 'fontWeight': 800}}>*</span>
+    ) : '';
+
+    return (
+      <span>{nodeName}{unsavedAsterisk}</span>
+    );
+  }
+
+  tableOnRowSelect = (row, isSelected) => {
+    // force at least 1 node to be selected at all times
     selectNodes({
-      nodes: [node]
+      nodes: [row.name]
     });
   }
 
-  renderNodeList = () => {
-    const {nodes, selectedNodes, nodesWithDrafts} = this.props;
-    const selectedNodeNames = new Set(selectedNodes);
-    const nodesWithDraftsSet = new Set(nodesWithDrafts);
-
-    // TODO: filter
-    return nodes.map((node) => {
-      const unsavedAsterisk = nodesWithDraftsSet.has(node) ? (
-        <span style={{color: '#cc0000', 'fontWeight': 800}}>*</span>
-      ) : '';
-
-      return (
-        <li
-          className={classNames(
-            'nc-node',
-            {'nc-node-selected': selectedNodeNames.has(node)}
-          )}
-          onClick={() => this.selectNode(node)}
-        >
-          {node}{unsavedAsterisk}
-        </li>
-      );
+  renderNodeTable = () => {
+    const nodesForTable = this.props.nodes.map(node => {
+      return {name: node};
     });
+
+    const selectRowProp = {
+      mode: 'radio',
+      clickToSelect: true,
+      hideSelectColumn: true,
+      bgColor: 'rgb(183,210,255)',
+      onSelect: this.tableOnRowSelect,
+      selected: this.props.selectedNodes,
+    };
+
+    return (
+      <BootstrapTable
+        tableStyle={{margin: 0}}
+        data={ nodesForTable }
+        bordered={ false }
+        selectRow={selectRowProp}
+      >
+        <TableHeaderColumn
+          dataField='name'
+          isKey={true}
+          dataSort={true}
+          filter={{
+            type: 'TextFilter',
+            placeholder: 'Filter Nodes',
+          }}
+          dataFormat={this.formatNodeName}
+        >Node name</TableHeaderColumn>
+      </BootstrapTable>
+    );
   }
 
   render() {
     return (
       <div className='rc-network-config-nodes'>
-        <ul>
-          {this.renderNodeList()}
-        </ul>
+        {this.renderNodeTable()}
       </div>
     );
   }
