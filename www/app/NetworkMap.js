43ec1771e1782f4d68d037c3f205b3738b6361a8
Kelvin Jiang <kelvinjiang@fb.com>
2017-12-03 23:08:00 -0800

    why u do dis

--- www/app/NetworkMap.js
+++ www/app/NetworkMap.js
@@ -380,6 +380,60 @@ export default class NetworkMap extends React.Component {
 
   getSiteMarker(pos, color, siteIndex): ReactElement<any> {
     let radiusByZoomLevel = this.state.zoomLevel - 9;
+    // const options = {
+    //   data: {
+    //     'dataPoint1': Math.random() * 20,
+    //     'dataPoint2': Math.random() * 20,
+    //     'dataPoint3': Math.random() * 20,
+    //     'dataPoint4': Math.random() * 20
+    //   },
+    //   chartOptions: {
+    // 		'dataPoint1': {
+    // 			fillColor: '#FEE5D9',
+    // 			minValue: 0,
+    // 			maxValue: 20,
+    // 			maxHeight: 20,
+    // 			displayText: function (value) {
+    // 				return value.toFixed(2);
+    // 			}
+    // 		},
+    // 		'dataPoint2': {
+    // 			fillColor: '#FCAE91',
+    // 			minValue: 0,
+    // 			maxValue: 20,
+    // 			maxHeight: 20,
+    // 			displayText: function (value) {
+    // 				return value.toFixed(2);
+    // 			}
+    // 		},
+    // 		'dataPoint3': {
+    // 			fillColor: '#FB6A4A',
+    // 			minValue: 0,
+    // 			maxValue: 20,
+    // 			maxHeight: 20,
+    // 			displayText: function (value) {
+    // 				return value.toFixed(2);
+    // 			}
+    // 		},
+    // 		'dataPoint4': {
+    // 			fillColor: '#CB181D',
+    // 			minValue: 0,
+    // 			maxValue: 20,
+    // 			maxHeight: 20,
+    // 			displayText: function (value) {
+    // 				return value.toFixed(2);
+    // 			}
+    // 		}
+  	//   },
+  	//   weight: 1,
+  	//   color: '#000000',
+    // };
+    console.log(this.refs.map.leafletElement);
+    // new Leaflet.PieChartMarker(
+    //   new LatLng(pos[0], pos[1]),
+    //   options
+    // ).addTo(this.refs.map.leafletElement);
+    // Leaflet.PieChartMarker(<LatLng> LatLng, <Chart options> options?);
     return (
       <CircleMarker center={pos}
         radius={MapDimensions[this.props.mapDimType].SITE_RADIUS}
