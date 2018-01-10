c0021e44cbdcd19e733c192b484cfc08ad3f7df0
Mike Nugent <mnugent@fb.com>
2018-01-10 11:46:19 -0800

    docker install pieces

--- schema/cxl.sql
+++ schema/cxl.sql
@@ -15,6 +15,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- Print on run so we can see it's executing properly in logs
+SELECT 'Running mysql init script' AS '';
+-- Move the DB creation into the config instead of the command line
+CREATE DATABASE IF NOT EXISTS `cxl`;
+USE cxl;
+
 --
 -- Table structure for table `alerts`
 --
