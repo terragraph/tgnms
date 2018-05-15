e277ba3a7c53f06fcefb01f1f69da4149381d75c
Paul M <pm@fb.com>
2018-05-15 16:28:28 -0700

    Add missing change for QueryHandler bug
    Meant to include this in git add -p...

--- beringei/beringei/tools/query_service/handlers/QueryHandler.h
+++ beringei/beringei/tools/query_service/handlers/QueryHandler.h
@@ -42,8 +42,7 @@ class QueryHandler : public proxygen::RequestHandler {
   void onError(proxygen::ProxygenError err) noexcept override;
 
  private:
-  std::shared_ptr<BeringeiConfigurationAdapterIf> configurationAdapter_;
-  std::shared_ptr<BeringeiClient> beringeiClient_;
+  bool receivedBody_;
   std::unique_ptr<folly::IOBuf> body_;
 };
 }
