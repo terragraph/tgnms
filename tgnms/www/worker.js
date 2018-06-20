87c186ec2ebb96b11b8e8a2e55a507bd352e4e00
Josh Braegger <jbraeg@fb.com>
2018-06-20 15:55:55 -0700

    Add axios worker retries
    Fetching data should be retried (it's idempotent, so it's okay)

--- tgnms/www/worker.js
+++ tgnms/www/worker.js
@@ -26,60 +26,38 @@ process.on('message', msg => {
     case 'poll':
       // expect a list of IP addresses
       msg.topologies.forEach(topology => {
-        apiServiceRequest(topology, 'getTopology')
-          .then(([success, responseTime, data]) => {
+        const getSuccessHandler = function(type, fieldName) {
+          return ([success, responseTime, data]) => {
             process.send({
               name: topology.name,
-              type: 'topology_update',
-              success: success,
-              response_time: responseTime,
-              topology: success ? data : null,
-              pizza: "time",
-            });
-          });
-        apiServiceRequest(topology, 'getCtrlStatusDump')
-          .then(([success, responseTime, data]) => {
-            process.send({
-              name: topology.name,
-              type: 'status_dump_update',
-              success: success,
-              response_time: responseTime,
-              status_dump: success ? data : null,
-            });
-          });
-        apiServiceRequest(topology, 'getIgnitionState')
-          .then(([success, responseTime, data]) => {
-            // which links are ignition candidates
-            process.send({
-              name: topology.name,
-              type: 'ignition_state',
-              success: success,
-              response_time: responseTime,
-              ignition_state: success ? data : null,
-            });
-          });
-        apiServiceRequest(topology, 'getUpgradeState')
-          .then(([success, responseTime, data]) => {
-            process.send({
-              name: topology.name,
-              type: 'upgrade_state',
-              success: success,
-              response_time: responseTime,
-              upgradeState: success ? data : null,
-            });
-          });
-        apiServiceRequest(topology, 'getHighAvailabilityState')
-          .then(([success, responseTime, data]) => {
-            // recvmsg.mType = BSTAR_FSM
-            process.send({
-              name: topology.name,
-              type: 'bstar_state',
+              type: type,
               success: success,
               response_time: responseTime,
               controller_ip: topology.controller_ip_active,
-              bstar_fsm: success ? data : null,
+              [fieldName]: success ? data : null,
             });
-          });
+          };
+        };
+        const getErrorHandler = function(type) {
+          return (error) => {
+              console.error(error);
+          };
+        };
+        apiServiceRequest(topology, 'getTopology')
+          .then(getSuccessHandler('topology_update', 'topology'))
+          .catch(getErrorHandler('topology_update'));
+        apiServiceRequest(topology, 'getCtrlStatusDump')
+          .then(getSuccessHandler('status_dump_update', 'status_dump'))
+          .catch(getErrorHandler('status_dump_update'));
+        apiServiceRequest(topology, 'getIgnitionState')
+          .then(getSuccessHandler('ignition_state', 'ignition_state'))
+          .catch(getErrorHandler('ignition_state'));
+        apiServiceRequest(topology, 'getUpgradeState')
+          .then(getSuccessHandler('upgrade_state', 'upgradeState'))
+          .catch(getErrorHandler('upgrade_state'));
+        apiServiceRequest(topology, 'getHighAvailabilityState')
+          .then(getSuccessHandler('bstar_state', 'bstar_fsm'))
+          .catch(getErrorHandler('bstar_state'));
         if (topology.controller_ip_passive) {
           apiServiceRequest(topology, 'BStarGetState')
             .then(([success, responseTime, data]) => {
@@ -1558,7 +1536,7 @@ function apiServiceRequest(topology, apiMethod, data, config) {
   return new Promise((resolve, reject) => {
     const startTimer = new Date();
     const url = `${baseUrl}/api/${apiMethod}`;
-    axios.post(url, postData, config).then(response => {
+    retryAxios([100, 500, 1000], axios.post, url, postData, config).then(response => {
       const endTimer = new Date();
       const responseTime = endTimer - startTimer;
       const success = true;
@@ -1567,7 +1545,7 @@ function apiServiceRequest(topology, apiMethod, data, config) {
     .catch(error => {
       if (error.response) {
         console.error('Received status ' + error.response.status + 
-		      ' for url ' + url);
+                      ' for url ' + url);
       } else {
         console.error(error.message);
       }
@@ -1579,6 +1557,27 @@ function apiServiceRequest(topology, apiMethod, data, config) {
     });
   });
 }
+
+const retryAxios = async (delays, axiosFunc, ...axiosArgs) => {
+  // Extract the iterator from the iterable.
+  const timeout = ms => new Promise(res => setTimeout(res, ms))
+  const iterator = delays[Symbol.iterator]();
+  while (true) {
+    try {
+      // Always call the service at least once.
+      return await axiosFunc(...axiosArgs);
+    } catch (error) {
+      const { done, value } = iterator.next();
+      if (!done && error.response && error.response.status === 400) {
+        console.log('retrying ' + value);
+        await timeout(value);
+      } else {
+        // The error is not retriable or the iterable is exhausted.
+        throw error;
+      }
+    }
+  }
+};
 
 module.exports = {
   AggregatorProxy,
