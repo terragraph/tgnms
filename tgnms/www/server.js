e0006a1833c128fccdb0e13e3b49ed1a200a7148
Josh Braegger <jbraeg@fb.com>
2018-06-26 17:01:27 -0700

    Misc cleanup in server.js

--- tgnms/www/server.js
+++ tgnms/www/server.js
@@ -11,36 +11,24 @@ const {
   reloadInstanceConfig,
 } = require('./server/topology/model');
 const topologyPeriodic = require('./server/topology/periodic');
-const topologyTTypes = require('./thrift/gen-nodejs/Topology_types');
+
+const compression = require('compression');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
-// set up font awesome here
-
-const compression = require('compression');
-
-// packaging
 const webpack = require('webpack');
+
 const devMode = process.env.NODE_ENV !== 'production';
 const port = devMode && process.env.PORT ? process.env.PORT : 80;
 
 const app = express();
+
 app.use(compression());
-const queryHelper = require('./queryHelper');
-const ipaddr = require('ipaddr.js');
-const pty = require('pty.js');
 
-
-var fbinternal = {};
-
-// serve static js + css
-app.use('/static', express.static(path.join(__dirname, 'static')));
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-const expressWs = require('express-ws')(app);
-
+app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use('/apiservice', require('./server/apiservice/routes'));
 app.use('/controller', require('./server/controller/routes'));
 app.use('/map', require('./server/map/routes'));
@@ -51,7 +39,6 @@ app.use('/topology', require('./server/topology/routes'));
 // First-time stuff
 reloadInstanceConfig();
 topologyPeriodic.startPeriodicTasks();
-
 
 if (devMode) {
   // serve developer, non-minified build
