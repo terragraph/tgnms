5ef22b3567f0e7792448ea2677afb3ca88122d95
Jeffrey Han <jeffreyhan@fb.com>
2018-07-09 19:58:22 -0700

    Fix duplicate IDs on alarms.

--- alarms/src/com/terragraph/alarms/Alarm.java
+++ alarms/src/com/terragraph/alarms/Alarm.java
@@ -18,13 +18,22 @@ public class Alarm implements Serializable {
 
     /** Alarm event type. */
     public enum EventType {
-        HEARTBEAT,
-        NETWORK_DN,
-        NETWORK_CN,
-        NODE_DN,
-        NODE_POP,
-        LINK_DN2DN,
-        CONTROLLER_PRIMARY
+        HEARTBEAT(0),
+        NETWORK_DN(1),
+        NETWORK_CN(2),
+        NODE_DN(3),
+        NODE_POP(4),
+        LINK_DN2DN(5),
+        CONTROLLER_PRIMARY(6);
+
+        /** A unique identifier for this event type. */
+        private final int id;
+
+        /** Constructor. */
+        EventType(int id) { this.id = id; }
+
+        /** Returns the unique event identifier. */
+        public int getId() { return id; }
     }
 
     /** Alarm type. */
@@ -55,7 +64,7 @@ public class Alarm implements Serializable {
     private final AlarmType alarmType;
 
     /**
-     * Creates an alarm.
+     * Creates a new alarm.
      * @param severity The alarm severity
      * @param eventType The event triggering this alarm
      * @param description The description of the event
@@ -65,8 +74,8 @@ public class Alarm implements Serializable {
     }
 
     /**
-     * Creates an alarm.
-     * @param id If non-null, the alarm ID; otherwise ID is auto-assigned
+     * Creates an alarm with the given parameters.
+     * @param id If non-null, the alarm ID; otherwise ID is auto-assigned based on the system time and event type
      * @param severity The alarm severity
      * @param eventType The event triggering this alarm
      * @param description The description of the event
@@ -79,7 +88,7 @@ public class Alarm implements Serializable {
         } else {
             this.id = (eventType == EventType.HEARTBEAT)
                 ? String.format("%s%s", ID_PREFIX, HEARTBEAT_ID)
-                : String.format("%s%d", ID_PREFIX, ts);
+                : String.format("%s%d-%d", ID_PREFIX, eventType.getId(), ts);
         }
         this.severity = severity;
         this.eventType = eventType;
