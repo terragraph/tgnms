6f6ec77cd220378fb78354d46faa015da378d4ca
Xu Du <xud@devvm18912.prn1.facebook.com>
2018-07-31 08:35:06 -0700

    first version with tests

--- analytics/crontest/cron_mgmt.py
+++ analytics/crontest/cron_mgmt.py
@@ -1,39 +1,91 @@
 #!/usr/bin/env python3
 
 """
-   Provide functions that can submit, view, remove linux Cron jobs via crontab.
+   Provide CronMgmt class that can submit, view, remove linux Cron jobs via crontab.
 """
 
 import datetime
 import logging
+
 from crontab import CronTab
 
 
 class CronMgmt(object):
     """
-    Provide CrontabMgmt class which will be used to submit, view, and remove Linux
+    Provide CronMgmt class which will be used to submit, view, and remove periodic
     cron jobs.
     """
 
     def __init__(self):
         self.comment = "NMS-Analytics"
         self.cron = CronTab(user="root")
-        self.log = open("/root/crontest/append.txt", "a")
-        self.log.write("\nAccessed on " + str(datetime.now()))
+        self.log = "/root/terragraph-nms/analytics/crontest/append.txt"
 
     def schedule_jobs_every_minutes(self, command, period_in_min=1):
+        """
+        Submit periodic cron jobs.
+
+        Args:
+        command: string, command to be run.
+        period_in_min: the period of the submitted job in minutes.
+
+        Return:
+        void.
+        """
         job = self.cron.new(command=command, comment=self.comment)
-        # job.minute.on(0, 15, 30, 60)
-        job.minute.every(period_in_min)
+        if period_in_min < 1:
+            logging.error("Analytics should not submit sub-min jobs")
+            return
+        elif period_in_min > 60:
+            logging.error(
+                "schedule_jobs_every_minutes() should not submit jobs with"
+                + "period larger than 1 hour"
+            )
+            return
+        else:
+            period_in_min = int(period_in_min)
+            job.minute.every(period_in_min)
+
         job.enable()
         self.cron.write()
-        self.log.write(str(datetime.now()), "Schedule Command")
+        try:
+            with open(self.log, "a") as file:
+                file.writelines(
+                    str(datetime.datetime.now())
+                    + "Submit Command '{}' with period_in_min of {}\n".format(
+                        command, period_in_min
+                    )
+                )
+        except EnvironmentError:
+            logging.error("Cannot open log output file ", self.log)
 
-    def show_all(self):
-        logging.warning(self.cron.render())
+    def show_current(self):
+        """
+        Return the current submitted Analytics cron jobs.
+
+        Return:
+        List of submitted Analytics jobs. Each job is represented by a string.
+        """
+        jobs = []
+        for job in self.cron:
+            if job.comment == self.comment:
+                jobs.append(repr(job))
+
+        return jobs
 
     def remove_all(self):
+        """
+        Remove all current submitted Analytics cron jobs.
+        """
         for job in self.cron:
             if job.comment == self.comment:
                 self.cron.remove(job)
                 self.cron.write()
+                self.cron.write()
+        try:
+            with open(self.log, "a") as file:
+                file.writelines(
+                    str(datetime.datetime.now()) + "Cleared all Analytics jobs\n"
+                )
+        except EnvironmentError:
+            logging.error("Cannot open log output file ", self.log)
--- analytics/crontest/remove_current_corn.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from crontab import CronTab
-
-my_cron = CronTab(user='root')
-for job in my_cron:
-    if job.comment == 'nms-analytics':
-        my_cron.remove(job)
-        my_cron.write()
