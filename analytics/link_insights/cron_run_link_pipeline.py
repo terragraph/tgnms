a87b26f2cec9021dbc6dd7f618e4e1ee3e8aa2e1
Xu Du <xud@devvm18912.prn1.facebook.com>
2018-08-01 17:48:10 -0700

    address comment

--- analytics/link_insights/cron_run_link_pipeline.py
+++ analytics/link_insights/cron_run_link_pipeline.py
@@ -10,26 +10,26 @@ import json
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from module.cron_mgmt import CronMgmt
+from module.path_store import PathStore
 
 
-def cron_run_link_pipeline(pipeline_jobs):
+def cron_run_link_pipeline():
     """ Run the link insights pipelines.
-
-        Args:
-        pipeline_jobs: a dictionary of cron jobs to submit, from the AnalyticsConfig.
-
-        Return:
-        void.
     """
+
+    with open(PathStore.ANALYTICS_CONFIG_FILE) as config_file:
+        analytics_config = json.load(config_file)
+    pipeline_jobs = analytics_config["pipelines"]
 
     # Submit jobs via cron_mgmt
     cron_mgmt = CronMgmt()
     for job in pipeline_jobs:
+        command = pipeline_jobs[job]["command"].format(PathStore.ANALYTICS_DIR)
         logging.info(
-            "Submitting job with command '{}'".format(pipeline_jobs[job]["command"])
+            "Submitting job with command '{}'".format(command)
         )
         cron_mgmt.schedule_jobs_every_minutes(
-            pipeline_jobs[job]["command"],
+            command,
             period_in_min=pipeline_jobs[job]["period_in_mins"],
         )
 
@@ -43,13 +43,7 @@ if __name__ == "__main__":
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
-    analytics_config_file = "../AnalyticsConfig.json"
     try:
-        with open(analytics_config_file) as config_file:
-            analytics_config = json.load(config_file)
-
-        pipeline_jobs = analytics_config["pipelines"]
-
-        cron_run_link_pipeline(pipeline_jobs)
+        cron_run_link_pipeline()
     except BaseException as err:
         logging.error("Fail to submit periodic jobs with error '{}' ".format(err.args))
