d627714b33520a8a193ee32b177510469a755cd7
Paul McCutcheon <pm@fb.com>
2018-09-20 13:27:47 -0700

    add django to tg nms analytics image
    Summary:
    Tried on remote host (since wget on github/thrift fails in lab).
    Image built fine. 'pip install' vs 'python3 -m pip install' seems to be the
    same. If I do pip install, python3 -m pip install shows it's already
    installed to the same path.
    
    Reviewed By: anubhavsingla
    
    Differential Revision: D9976430
    
    fbshipit-source-id: bd2bd8e27b39cd32bcca2b92da90157a6746b4a4

--- analytics/setup_analytics_env.sh
+++ analytics/setup_analytics_env.sh
@@ -5,18 +5,21 @@ echo "This script configures ubuntu with everything needed to run NMS analytics.
 echo "It requires that you run it as root. sudo works great for that."
 
 APACHE_THRIFT_VERSION="0.11.0"
-REQUESTS_VERSION="2.19.1"
-NUMPY_VERSION="1.14.5"
-MATPLOTLIB_VERSION="2.2.2"
-PYMYSQL_VERSION="0.9.2"
+# Python package versions
+DJANGO_VERSION="2.1.1"
 JUPYTER_VERSION="1.0.0"
+MATPLOTLIB_VERSION="2.2.2"
+NUMPY_VERSION="1.14.5"
+PYMYSQL_VERSION="0.9.2"
+REQUESTS_VERSION="2.19.1"
 
 # Install Python3 packages
+pip install django==$DJANGO_VERSION
+pip install jupyter==$JUPYTER_VERSION
+pip install matplotlib==$MATPLOTLIB_VERSION
 pip install numpy==$NUMPY_VERSION
+pip install PyMySQL==$PYMYSQL_VERSION
 pip install requests==$REQUESTS_VERSION
-python3 -m pip install matplotlib==$MATPLOTLIB_VERSION
-python3 -m pip install PyMySQL==$PYMYSQL_VERSION
-python3 -m pip install jupyter==$JUPYTER_VERSION
 
 # Install Apache Thrift
 cd /tmp
