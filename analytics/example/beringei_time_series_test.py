928ddfc1228552eb0700317397a9e7f8fbdd40ca
Anubhav Singla <anub@fb.com>
2018-10-09 14:08:52 -0700

    add beringei_time_series module
    Summary: Add a module which hides details of RawReadBeringeiData and other BQS APIs and provide simpler interface. This module does not know anything about topology file - it deals with mac addresses.
    
    Reviewed By: csmodlin
    
    Differential Revision: D9983053
    
    fbshipit-source-id: 65251363314e44402a1ea1d66b8059d9c5c74335

---
+++ analytics/example/beringei_time_series_test.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+"""
+Test examples for beringei_time_series module
+"""
+import logging
+import math
+import os
+import sys
+import time
+import unittest
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import module.beringei_time_series as bts
+from module.topology_handler import fetch_network_info
+
+
+class BeringeiTimeSeriesTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(BeringeiTimeSeriesTest, self).__init__(*args, **kwargs)
+        # the tests can fail due to
+        # if "unittest" does not exist, write won't work the first time
+        # if data points already exist, the write won't work for that time bin
+        logging.warning("Tests may fail the first time or when run back to back")
+        t = fetch_network_info()
+        t = t[list(t.keys())[0]]["topology"]
+        node_name_to_mac = {n["name"]: n["mac_addr"] for n in t["nodes"]}
+        self._src_mac = node_name_to_mac[t["links"][0]["a_node_name"]]
+        self._peer_mac = node_name_to_mac[t["links"][0]["z_node_name"]]
+        self._topology_name = t["name"]
+        self._interval = 30
+        self._end_time = math.floor(int(time.time()) / self._interval) * self._interval
+        self._start_time = self._end_time - 90
+        self._name = "unittest"
+        self._times = list(range(self._start_time, self._end_time, self._interval))
+        self._values = [float(i) for i in range(len(self._times))]
+
+    def test_write_read_node_stat(self):
+        # write ramp time series
+        ramp_ts = bts.TimeSeries(
+            self._values,
+            self._times,
+            self._name,
+            self._topology_name,
+            src_mac=self._src_mac,
+        )
+        bts.write_time_series_list([ramp_ts], [self._interval])
+        # read time series
+        tsl = bts.read_time_series_list(
+            self._name,
+            [self._src_mac],
+            [],
+            self._start_time,
+            self._end_time,
+            self._interval,
+            self._topology_name,
+        )
+        # compare
+        self.assertEqual(len(tsl), 1)
+        self.assertEqual(tsl[0], ramp_ts)
+
+    def test_write_read_link_stat(self):
+        # write ramp time series
+        ramp_ts = bts.TimeSeries(
+            self._values,
+            self._times,
+            self._name,
+            self._topology_name,
+            src_mac=self._src_mac,
+            peer_mac=self._peer_mac,
+        )
+        bts.write_time_series_list([ramp_ts], [self._interval])
+        # read time series
+        tsl = bts.read_time_series_list(
+            self._name,
+            [self._src_mac],
+            [self._peer_mac],
+            self._start_time,
+            self._end_time,
+            self._interval,
+            self._topology_name,
+        )
+        # compare
+        self.assertEqual(len(tsl), 1)
+        self.assertEqual(tsl[0], ramp_ts)
+        pass
+
+    def test_write_read_network_stat(self):
+        # write ramp time series
+        ramp_ts = bts.TimeSeries(
+            self._values,
+            self._times,
+            self._name,
+            self._topology_name,
+        )
+        bts.write_time_series_list([ramp_ts], [self._interval])
+        # reading aggregated stats is not supported, check ui
+        pass
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    unittest.main()
---
+++ analytics/module/beringei_time_series.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+
+"""
+Read/Writes List of TimeSeries to Beringei database
+"""
+
+import logging
+import os
+import sys
+from typing import Optional, List
+from module.beringei_db_access import BeringeiDbAccess
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..") + "/interface/gen-py")
+)
+from facebook.gorilla.beringei_query import ttypes as bq
+
+
+class TimeSeries(object):
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, TimeSeries):
+            return (
+                self.values == other.values
+                and self.times == other.times
+                and self.name == other.name
+                and self.src_mac == other.src_mac
+                and self.peer_mac == other.peer_mac
+                and self.topology == other.topology
+            )
+        return False
+
+    def __repr__(self):
+        return "times={}, values={}, name={}, src_mac={}, peer_mac={}, topology={}".format(
+            self.times,
+            self.values,
+            self.name,
+            self.src_mac,
+            self.peer_mac,
+            self.topology,
+        )
+
+    def __init__(
+        self,
+        values: List,
+        times: List,
+        name: str,
+        topology: str,
+        src_mac: Optional[str] = None,
+        peer_mac: Optional[str] = None,
+    ) -> None:
+        assert len(times) == len(values)
+        self.times = times
+        self.values = values
+        self.name = name
+        self.src_mac = src_mac
+        self.peer_mac = peer_mac
+        self.topology = topology
+
+
+# each RawReadQuery can contain multiple RawQueryKey
+# but use only one RawQueryKey
+# due to bug in Beringei or BQS/RawReadBeringeiData
+def __get_copy_rrq(rrq: bq.RawReadQuery) -> bq.RawReadQuery:
+    copy_rrq = bq.RawReadQuery()
+    copy_rrq.startTimestamp = rrq.startTimestamp
+    copy_rrq.endTimestamp = rrq.endTimestamp
+    copy_rrq.interval = rrq.interval
+    copy_rrq.queryKeyList = [bq.RawQueryKey()]
+    copy_rrq.queryKeyList[0].topologyName = rrq.queryKeyList[0].topologyName
+    copy_rrq.queryKeyList[0].metricName = rrq.queryKeyList[0].metricName
+    copy_rrq.queryKeyList[0].sourceMac = rrq.queryKeyList[0].sourceMac
+    copy_rrq.queryKeyList[0].peerMac = rrq.queryKeyList[0].peerMac
+    return copy_rrq
+
+
+def read_time_series_list(
+    name: str,
+    src_macs: List[str],
+    peer_macs: List[str],
+    start_time: int,
+    end_time: int,
+    interval: int,
+    topology_name: str,
+) -> List[TimeSeries]:
+    src_macs = [m.lower() for m in src_macs]
+    peer_macs = [m.lower() for m in peer_macs]
+    name = name.lower()
+    if len(peer_macs):
+        assert len(src_macs) == len(peer_macs)
+    rrqr = bq.RawReadQueryRequest()
+    rrqr.queries = []
+    rrq = bq.RawReadQuery()
+    rrq.startTimestamp = start_time
+    rrq.endTimestamp = end_time
+    rrq.queryKeyList = [bq.RawQueryKey()]
+    rrq.interval = interval
+    rrq.queryKeyList[0].topologyName = topology_name
+    rrq.queryKeyList[0].metricName = name
+    if src_macs:
+        for i in range(len(src_macs)):
+            rrq.queryKeyList[0].sourceMac = src_macs[i]
+            if len(peer_macs):
+                rrq.queryKeyList[0].peerMac = peer_macs[i]
+            else:
+                rrq.queryKeyList[0].peerMac = None
+            rrqr.queries.append(__get_copy_rrq(rrq))
+    else:
+        # unsupported in BQS
+        rrq.queryKeyList[0].sourceMac = None
+        rrq.queryKeyList[0].peerMac = None
+        rrqr.queries.append(__get_copy_rrq(rrq))
+
+    # execute query and convert non-empty time-series to list
+    try:
+        resp = BeringeiDbAccess().read_beringei_db(rrqr)
+    except Exception as e:
+        logging.error("Exception happened while reading beringei db")
+        logging.warning("Exception: {}".format(e))
+        return []
+
+    time_series_list = []
+    for q in range(len(rrqr.queries)):
+        for k in range(len(rrqr.queries[q].queryKeyList)):
+            ts = resp.queryReturnList[q].timeSeriesAndKeyList[k].timeSeries
+            if len(ts):
+                time_series = TimeSeries(
+                    values=[p.value for p in ts],
+                    times=[p.unixTime for p in ts],
+                    name=rrqr.queries[q].queryKeyList[k].metricName,
+                    src_mac=rrqr.queries[q].queryKeyList[k].sourceMac,
+                    peer_mac=rrqr.queries[q].queryKeyList[k].peerMac,
+                    topology=rrqr.queries[q].queryKeyList[k].topologyName,
+                )
+                time_series_list.append(time_series)
+
+    return time_series_list
+
+
+def write_time_series_list(
+    time_series_list: List[TimeSeries], intervals: List[int]
+) -> None:
+    node_stats = []
+    agg_stats = []
+    for t in time_series_list:
+        if t.src_mac:
+            key = t.name
+            if t.peer_mac:
+                key = "link.{}.{}".format(t.peer_mac, t.name)
+        else:
+            key = "network.{}".format(t.name)
+        stats = []
+        for i in range(len(t.values)):
+            stats.append(bq.Stat(key=key, ts=t.times[i], value=t.values[i]))
+        if t.src_mac:
+            node_stats.append(bq.NodeStats(mac=t.src_mac, stats=stats))
+        else:
+            agg_stats.append(bq.AggStats(topologyName=t.topology, stats=stats))
+    uwr = bq.UnifiedWriteRequest(
+        intervals=intervals, nodeStats=node_stats, aggStats=agg_stats
+    )
+
+    try:
+        BeringeiDbAccess().write_node_and_agg_stats_beringei_db(uwr)
+    except Exception as e:
+        logging.error("Exception happened while writing beringei db")
+        logging.warning("Exception: {}".format(e))
