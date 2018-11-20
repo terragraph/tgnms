e769c9b0f7235844643ab10dc02997e576d28cfa
Paul McCutcheon <pm@fb.com>
2018-11-19 16:29:45 -0800

    fix analytic's broken thrift generation
    Summary:
    This thrift generation is *INSANE*.
    
    The new thrift files didn't include namespace cpp2, so this removes some random lines.
    
    Reviewed By: michaelnugent
    
    Differential Revision: D13131750
    
    fbshipit-source-id: 7e9ba5a49f3d948598c1d2a958957165f7e4d8db

--- analytics/interface/generate_analytics_api.sh
+++ analytics/interface/generate_analytics_api.sh
@@ -5,70 +5,20 @@ SRC_DIR=$(dirname "$0")
 SRC_DIR+="/beringei/if"
 DST_DIR=$(dirname "$0")
 
-BERINGEI_DATA_THRIFT="beringei_data.thrift"
-BERINGEI_QUERY_THRIFT="beringei_query.thrift"
-TOPOLOGY_THRIFT="Topology.thrift"
-SCAN_THRIFT="scans.thrift"
-CONTROLLER_THRIFT="Controller.thrift"
-BWALLOCATION_THRIFT="BWAllocation.thrift"
-IPPREFIX_THRIFT="IpPrefix.thrift"
-LSDB_THRIFT="Lsdb.thrift"
+# order to generate compiled thrift files
+THRIFT_FILES="beringei_data.thrift beringei_query.thrift Topology.thrift scans.thrift Controller.thrift BWAllocation.thrift IpPrefix.thrift Lsdb.thrift"
 
-cp "$SRC_DIR/$BERINGEI_DATA_THRIFT" "$DST_DIR"
-cp "$SRC_DIR/$BERINGEI_QUERY_THRIFT" "$DST_DIR"
-cp "$SRC_DIR/$TOPOLOGY_THRIFT" "$DST_DIR"
-cp "$SRC_DIR/$SCAN_THRIFT" "$DST_DIR"
-cp "$SRC_DIR/$CONTROLLER_THRIFT" "$DST_DIR"
-cp "$SRC_DIR/$BWALLOCATION_THRIFT" "$DST_DIR"
-cp "$SRC_DIR/$IPPREFIX_THRIFT" "$DST_DIR"
-cp "$SRC_DIR/$LSDB_THRIFT" "$DST_DIR"
+for file in $THRIFT_FILES
+do
+        cp "${SRC_DIR}/${file}" "${DST_DIR}"
+        sed -i '/namespace cpp2/d' "${DST_DIR}/${file}"
+done
 
-# remove the cpp2 lines for beringei_data.thrift
-_LINE_TO_REMOVE=$(grep -n "cpp2" $DST_DIR/$BERINGEI_DATA_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_TO_REMOVE}d" $DST_DIR/$BERINGEI_DATA_THRIFT
-# remove the cpp2 lines for beringei_query.thrift and fix the include path
-_LINE_TO_REMOVE=$(grep -n "cpp2" $DST_DIR/$BERINGEI_QUERY_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_TO_REMOVE}d" $DST_DIR/$BERINGEI_QUERY_THRIFT
-_LINE_OF_INCLUDE=$(grep -n "beringei/if/Topology.thrift" $DST_DIR/$BERINGEI_QUERY_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_OF_INCLUDE}d" $DST_DIR/$BERINGEI_QUERY_THRIFT
-NEW_INCLUDE_LINE="include \"Topology.thrift\""
-sed -i "${_LINE_OF_INCLUDE} a ${NEW_INCLUDE_LINE}" $DST_DIR/$BERINGEI_QUERY_THRIFT
-# remove the cpp2 lines for Topology.thrift
-_LINE_TO_REMOVE=$(grep -n "cpp2" $DST_DIR/$TOPOLOGY_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_TO_REMOVE}d" $DST_DIR/$TOPOLOGY_THRIFT
-# remove the cpp2 lines for scans.thrift
-_LINE_TO_REMOVE=$(grep -n "cpp2" $DST_DIR/$SCAN_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_TO_REMOVE}d" $DST_DIR/$SCAN_THRIFT
-# remove the cpp2 lines for Controller.thrift and fix the include path
-_LINE_TO_REMOVE=$(grep -n "cpp2" $DST_DIR/$CONTROLLER_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_TO_REMOVE}d" $DST_DIR/$CONTROLLER_THRIFT
-_LINE_OF_INCLUDE=$(grep -n "openr/if/Lsdb.thrift" $DST_DIR/$CONTROLLER_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_OF_INCLUDE}d" $DST_DIR/$CONTROLLER_THRIFT
-NEW_INCLUDE_LINE="include \"Lsdb.thrift\""
-sed -i "${_LINE_OF_INCLUDE} a ${NEW_INCLUDE_LINE}" $DST_DIR/$CONTROLLER_THRIFT
-# remove the cpp2 lines for BWAllocation.thrift
-_LINE_TO_REMOVE=$(grep -n "cpp2" $DST_DIR/$BWALLOCATION_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_TO_REMOVE}d" $DST_DIR/$BWALLOCATION_THRIFT
-# remove the cpp2 and php lines for IpPrefix.thrift
-_LINE_TO_REMOVE=$(grep -n "cpp2" $DST_DIR/$IPPREFIX_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_TO_REMOVE}d" $DST_DIR/$IPPREFIX_THRIFT
-_LINE_TO_REMOVE=$(grep -n "php" $DST_DIR/$IPPREFIX_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_TO_REMOVE}d" $DST_DIR/$IPPREFIX_THRIFT
-# remove the cpp2 and php lines for Lsdb.thrift
-_LINE_TO_REMOVE=$(grep -n "cpp2" $DST_DIR/$LSDB_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_TO_REMOVE}d" $DST_DIR/$LSDB_THRIFT
-_LINE_TO_REMOVE=$(grep -n "php" $DST_DIR/$LSDB_THRIFT | head -1 | cut -d: -f1)
-sed -i "${_LINE_TO_REMOVE}d" $DST_DIR/$LSDB_THRIFT
+echo Thrift files obtained from $SRC_DIR, now start data structure generation for python
 
-echo Thrift files obatined from $SRC_DIR, now start data structure generation \for python
-
-thrift --gen py -o $DST_DIR "$DST_DIR/$BERINGEI_DATA_THRIFT"
-thrift --gen py -o $DST_DIR "$DST_DIR/$BERINGEI_QUERY_THRIFT"
-thrift --gen py -o $DST_DIR "$DST_DIR/$TOPOLOGY_THRIFT"
-thrift --gen py -o $DST_DIR "$DST_DIR/$SCAN_THRIFT"
-thrift --gen py -o $DST_DIR "$DST_DIR/$CONTROLLER_THRIFT"
-thrift --gen py -o $DST_DIR "$DST_DIR/$BWALLOCATION_THRIFT"
-thrift --gen py -o $DST_DIR "$DST_DIR/$IPPREFIX_THRIFT"
-thrift --gen py -o $DST_DIR "$DST_DIR/$LSDB_THRIFT"
+for file in $THRIFT_FILES
+do
+        thrift --gen py -o $DST_DIR "${DST_DIR}/${file}"
+done
 
 echo Data structures successfully generated!
