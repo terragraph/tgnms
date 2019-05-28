185a65314b09335e4eb11b7562a0d44cd4432304
Cory Modlin <cmodlin@fb.com>
2019-05-28 09:20:53 -0700

    Add link_health short name to db
    Summary: Paul changed things so that short names are defined in the link_metric table in mysql.  This seeder file will add link_health to that table.  Link_health is used in D15425125 for map overlays.
    
    Reviewed By: pmccut
    
    Differential Revision: D15435172
    
    fbshipit-source-id: a22f58cafeb0ea0e8c5fba95ed77df611aaeb6ad

---
+++ tgnms/fbcnms-projects/tgnms/server/seeders/20190521102700-link_metrics.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, _Sequelize) => {
+    return queryInterface.bulkInsert('link_metric', [{
+      name: 'link_health',
+      key_name: 'health',
+      key_prefix: 'link',
+      description: 'Link Health Periodic Monitoring',
+    }]);
+  },
+
+  down: (_queryInterface, _Sequelize) => {
+  },
+};
