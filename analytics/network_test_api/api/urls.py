5d5c1e930ae4b50889501416ad68b775da7718d6
Cory Modlin <cmodlin@fb.com>
2019-05-29 13:22:42 -0700

    Unbreak Network Test
    Summary: I removed views.py from the repo but when I tested network test, it was still there.  Importing views.py crashes network test.  This diff removes the import to stop the crashes.
    
    Reviewed By: spurav
    
    Differential Revision: D15541798
    
    fbshipit-source-id: 269cf991435cb6b95b78be1b9e9de54b900fbe07

--- analytics/network_test_api/api/urls.py
+++ analytics/network_test_api/api/urls.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 # Copyright 2004-present Facebook. All Rights Reserved.
 
-from api import views
-from django.conf.urls import url
-
-
 urlpatterns = [
-    url(r"start_test/$", views.start_test, name="start_test"),
-    url(r"stop_test/$", views.stop_test, name="stop_test"),
-    url(r"help/$", views.help, name="help"),
 ]
