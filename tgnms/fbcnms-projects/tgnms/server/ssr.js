e2a84d763bbc49863e49cb71f90749461a8157d3
Alex Clavelle <clavelle@fb.com>
2019-07-23 10:31:51 -0700

    fix SSR
    Summary: fixes serverside rendering for the new api
    
    Reviewed By: tcirstea
    
    Differential Revision: D16292484
    
    fbshipit-source-id: bda846ec5a126196658ab3e0e17d78b4ced2ad94

--- tgnms/fbcnms-projects/tgnms/server/ssr.js
+++ tgnms/fbcnms-projects/tgnms/server/ssr.js
@@ -8,32 +8,30 @@
 'use strict';
 
 import * as React from 'react';
-import {JssProvider, SheetsRegistry} from 'react-jss';
-import {
-  MuiThemeProvider,
-  createGenerateClassName,
-  createMuiTheme,
-} from '@material-ui/core/styles';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import {ServerStyleSheets, StylesProvider} from '@material-ui/styles';
 import {renderToString} from 'react-dom/server';
 
 export function render(
   RootComponent: React.ComponentType<any>,
-  serverProps: Object,
+  serverProps: Object = {},
 ): {app: string, styleSheets: string} {
-  const sheets = new SheetsRegistry();
-  const generateClassName = createGenerateClassName();
-  const sheetsManager = new Map();
+  const sheets = new ServerStyleSheets();
+
   //TODO: use the same theme as the app
   const theme = createMuiTheme({});
   const app: string = renderToString(
-    <JssProvider registry={sheets} generateClassName={generateClassName}>
-      <MuiThemeProvider theme={theme} sheetsManager={sheetsManager}>
-        <RootComponent serverProps={serverProps} />
-      </MuiThemeProvider>
-    </JssProvider>,
+    sheets.collect(
+      <StylesProvider>
+        <MuiThemeProvider theme={theme}>
+          <RootComponent serverProps={serverProps} />
+        </MuiThemeProvider>
+      </StylesProvider>,
+    ),
   );
+  const styleSheets = sheets.toString();
   return {
-    app: app,
-    styleSheets: sheets.toString(),
+    app,
+    styleSheets,
   };
 }
