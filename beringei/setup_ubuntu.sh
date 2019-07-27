2301837fa10be800cbf965f67c899abca0e09968
Paul McCutcheon <pm@fb.com>
2019-07-26 20:54:11 -0700

    nms/beringei: add rdkafka/cppkafka to build
    Summary:
    Adding kafka support requires us to bump the zstd version to keep it supported with rdkafka, which required building the libraries with the system versions instead of the versions we compile in /usr/local/facebook*.
    This will be easier to deal with once beringei code is gone, since it'll only be our code that we need to bring up support for with dependencies.
    
    Also deleting cmake temp files and all of the library code - which keeps the shared objects to link against.
    
    Reviewed By: elludraon
    
    Differential Revision: D16531213
    
    fbshipit-source-id: e56585c135abaeef3e3d63e263c2b65d0c7a9030

--- beringei/setup_ubuntu.sh
+++ beringei/setup_ubuntu.sh
@@ -2,9 +2,11 @@
 set -e
 
 FB_VERSION="2017.07.10.00"
-ZSTD_VERSION="1.1.1"
+ZSTD_VERSION="1.4.1"
 CMAKE_VERSION="3.14.4"
 CURL_VERSION="7.64.1"
+RDKAFKA_VERSION="1.1.0"
+CPPKAFKA_VERSION="0.3.1"
 
 echo "This script configures ubuntu with everything needed to run beringei."
 echo "It requires that you run it as root. sudo works great for that."
@@ -54,36 +56,49 @@ ready_destdir() {
 mkdir -pv /usr/local/facebook-${FB_VERSION}
 ln -sfT /usr/local/facebook-${FB_VERSION} /usr/local/facebook
 
-export LDFLAGS="-L/usr/local/facebook/lib -Wl,-rpath=/usr/local/facebook/lib"
-export CPPFLAGS="-I/usr/local/facebook/include"
-
 cd /tmp
 
 wget -O /tmp/cmake-${CMAKE_VERSION}.tar.gz https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz
+wget -O /tmp/librdkafka-${RDKAFKA_VERSION}.tar.gz https://github.com/edenhill/librdkafka/archive/v${RDKAFKA_VERSION}.tar.gz
+wget -O /tmp/cppkafka-${CPPKAFKA_VERSION}.tar.gz https://github.com/mfontanini/cppkafka/archive/v${CPPKAFKA_VERSION}.tar.gz
 wget -O /tmp/curl-${CURL_VERSION}.tar.gz https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz
+wget -O /tmp/mstch-master.tar.gz https://github.com/no1msd/mstch/archive/master.tar.gz
+wget -O /tmp/zstd-${ZSTD_VERSION}.tar.gz https://github.com/facebook/zstd/archive/v${ZSTD_VERSION}.tar.gz
 wget -O /tmp/folly-${FB_VERSION}.tar.gz https://github.com/facebook/folly/archive/v${FB_VERSION}.tar.gz
 wget -O /tmp/wangle-${FB_VERSION}.tar.gz https://github.com/facebook/wangle/archive/v${FB_VERSION}.tar.gz
 wget -O /tmp/fbthrift-${FB_VERSION}.tar.gz https://github.com/facebook/fbthrift/archive/v${FB_VERSION}.tar.gz
 wget -O /tmp/proxygen-${FB_VERSION}.tar.gz https://github.com/facebook/proxygen/archive/v${FB_VERSION}.tar.gz
-wget -O /tmp/mstch-master.tar.gz https://github.com/no1msd/mstch/archive/master.tar.gz
-wget -O /tmp/zstd-${ZSTD_VERSION}.tar.gz https://github.com/facebook/zstd/archive/v${ZSTD_VERSION}.tar.gz
 
 tar xzvf cmake-${CMAKE_VERSION}.tar.gz
+tar xzvf librdkafka-${RDKAFKA_VERSION}.tar.gz
+tar xzvf cppkafka-${CPPKAFKA_VERSION}.tar.gz
 tar xzvf curl-${CURL_VERSION}.tar.gz
+tar xzvf mstch-master.tar.gz
+tar xzvf zstd-${ZSTD_VERSION}.tar.gz
 tar xzvf folly-${FB_VERSION}.tar.gz
 tar xzvf wangle-${FB_VERSION}.tar.gz
 tar xzvf fbthrift-${FB_VERSION}.tar.gz
 tar xzvf proxygen-${FB_VERSION}.tar.gz
-tar xzvf mstch-master.tar.gz
-tar xzvf zstd-${ZSTD_VERSION}.tar.gz
 
 PROC=`cat /proc/cpuinfo | grep processor | wc -l`
-echo ${PROC}
 
 pushd cmake-${CMAKE_VERSION}
 ./configure --prefix=/usr
 make -j ${PROC} install
 popd
+
+pushd librdkafka-${RDKAFKA_VERSION}
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/facebook-${FB_VERSION} -DBUILD_SHARED_LIBS:BOOL=ON .
+make -j ${PROC} install
+popd
+
+pushd cppkafka-${CPPKAFKA_VERSION}
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/facebook-${FB_VERSION} -DBUILD_SHARED_LIBS:BOOL=ON .
+make -j ${PROC} install
+popd
+
+export LDFLAGS="-L/usr/local/facebook/lib -Wl,-rpath=/usr/local/facebook/lib"
+export CPPFLAGS="-I/usr/local/facebook/include"
 
 pushd curl-${CURL_VERSION}
 ./configure
@@ -125,3 +140,6 @@ autoreconf -ivf
 ./configure --prefix=/usr/local/facebook-${FB_VERSION}
 make -j ${PROC} install
 popd
+
+# cleanup build
+rm -rf /tmp/*
