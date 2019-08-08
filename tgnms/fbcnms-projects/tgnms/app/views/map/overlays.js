111237904a84d406774549ebe451a3fe5f37848c
Devansh Kukreja <dkukreja@fb.com>
2019-08-08 15:51:46 -0700

    Hide Link Health Overlay with Prometheus Backend
    Summary: Removes the Link Health Overlay option if the backend is Prometheus. The link_health metric hasn't been migrated to prometheus yet. See Task #48050175 for more info and rationale.
    
    Reviewed By: aclave1
    
    Differential Revision: D16719174
    
    fbshipit-source-id: 7ec099a03f5c8e026cf05303438cdc9b24c47fe8

--- tgnms/fbcnms-projects/tgnms/app/views/map/overlays.js
+++ tgnms/fbcnms-projects/tgnms/app/views/map/overlays.js
@@ -46,13 +46,17 @@ const LINK_METRIC_OVERLAYS: {[string]: Overlay} = {
     type: 'ignition_status',
     id: 'ignition_status',
   },
-  link_health: {
-    name: 'Health',
-    type: 'metric',
-    id: 'link_health',
-    range: [0, 1, 2, 3, 4],
-    bounds: [0, 4],
-  },
+  ...(window.CONFIG.env.STATS_BACKEND === 'prometheus'
+    ? {}
+    : {
+        link_health: {
+          name: 'Health',
+          type: 'metric',
+          id: 'link_health',
+          range: [0, 1, 2, 3, 4],
+          bounds: [0, 4],
+        },
+      }),
   golay_tx: {name: 'Golay (TX)', type: 'golay', id: 'golay_tx'},
   golay_rx: {name: 'Golay (RX)', type: 'golay', id: 'golay_rx'},
   control_superframe: {
