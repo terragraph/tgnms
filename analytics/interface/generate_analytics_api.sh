973cdb221f3458f7e46612c40664532e0cefaa6b
Cory Modlin <cmodlin@fb.com>
2019-08-17 08:08:10 -0700

    Unbreak Jenkins analytics build
    Summary:
    Fix the analytics image build.
    
    requires: `cp -a $WORKSPACE/if $WORKSPACE/analytics`
    
    added to Jenkins
    
    if you guys prefer, i can move it to a directory in `interface` and then instead of `/../if` I can make it `/whatever/if`
    
    Reviewed By: cooperlees
    
    Differential Revision: D16869492
    
    fbshipit-source-id: 69ede49479eab1561b9b80e8b4c08dfb7d972d1f

--- analytics/interface/generate_analytics_api.sh
+++ analytics/interface/generate_analytics_api.sh
@@ -2,11 +2,11 @@
 set -e
 
 SRC_DIR=$(dirname "$0")
-SRC_DIR+="/beringei/if"
+SRC_DIR+="/../if"
 DST_DIR=$(dirname "$0")
 
 # order to generate compiled thrift files
-THRIFT_FILES="beringei_data.thrift beringei_query.thrift Topology.thrift scans.thrift Controller.thrift BWAllocation.thrift IpPrefix.thrift Lsdb.thrift Event.thrift NetworkTest.thrift"
+THRIFT_FILES="beringei_query.thrift Topology.thrift scans.thrift Controller.thrift BWAllocation.thrift IpPrefix.thrift Lsdb.thrift Event.thrift NetworkTest.thrift"
 
 for file in $THRIFT_FILES
 do
