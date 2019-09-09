da30ab0d5dcfee73a31940f77641138f575255b5
Omar Abdelkader <omikader@fb.com>
2019-09-09 09:15:47 -0700

    Fix test to return Object instead of NetworkDto
    Summary: Make sure to return untransformed data in mock function
    
    Reviewed By: aclave1
    
    Differential Revision: D17238677
    
    fbshipit-source-id: 4904987939b146b207bf3880a9a221f02ee8bdd5

--- tgnms/fbcnms-projects/tgnms/server/api/v1/__tests__/api.v1.test.js
+++ tgnms/fbcnms-projects/tgnms/server/api/v1/__tests__/api.v1.test.js
@@ -7,7 +7,6 @@
 
 import express from 'express';
 import request from 'supertest';
-import {NetworkDto} from '../../../../shared/dto/api/v1';
 const {
   getAllNetworkConfigs,
   getNetworkConfig,
@@ -17,6 +16,59 @@ jest.mock('../../../models');
 jest.mock('../../../topology/model');
 
 const configs = {
+  foo: {
+    name: 'foo',
+    backup: {
+      api_ip: '89c3:c57f:7d6f:b86b:f466:ca26:cf97:98ce',
+      api_port: 8080,
+      e2e_ip: null,
+      e2e_port: 7777,
+      id: 1,
+    },
+    controller_online: true,
+    id: 1,
+    offline_whitelist: {
+      links: {},
+      nodes: {},
+    },
+    primary: {
+      api_ip: '37:4249:58b2:ee57:cc1f:97a6:9c3e:456f',
+      api_port: 8080,
+      e2e_ip: null,
+      e2e_port: 7777,
+      id: 2,
+    },
+    query_service_online: true,
+    site_overrides: {},
+  },
+  bar: {
+    name: 'bar',
+    backup: {
+      api_ip: 'f0cc:efee:6ba2:312c:b254:7b3b:92e7:4064',
+      api_port: 8080,
+      e2e_ip: null,
+      e2e_port: 7777,
+      id: 3,
+    },
+    controller_online: true,
+    id: 2,
+    offline_whitelist: {
+      links: {},
+      nodes: {},
+    },
+    primary: {
+      api_ip: '8379:55f2:2371:91ee:c29f:d36a:1951:6817',
+      api_port: 8080,
+      e2e_ip: null,
+      e2e_port: 7777,
+      id: 4,
+    },
+    query_service_online: true,
+    site_overrides: {},
+  },
+};
+
+const responses = {
   foo: {
     name: 'foo',
     backup: {
@@ -68,8 +120,8 @@ test('networkContractTest', async () => {
     .get('/api/v1/network')
     .expect(200);
   expect(response.body.length).toBe(2);
-  expect(response.body[0]).toStrictEqual(configs.foo);
-  expect(response.body[1]).toStrictEqual(configs.bar);
+  expect(response.body[0]).toStrictEqual(responses.foo);
+  expect(response.body[1]).toStrictEqual(responses.bar);
 });
 
 test('networkNameContractTest', async () => {
@@ -81,7 +133,7 @@ test('networkNameContractTest', async () => {
   const response = await request(app)
     .get(`/api/v1/network/${networkName}`)
     .expect(200);
-  expect(response.body).toStrictEqual(configs[networkName]);
+  expect(response.body).toStrictEqual(responses[networkName]);
 });
 
 test('missingNetworkNameContractTest', async () => {
@@ -96,7 +148,7 @@ test('missingNetworkNameContractTest', async () => {
 });
 
 function mockGetAllNetworkConfigs() {
-  return Object.keys(configs).map(name => new NetworkDto(configs[name]));
+  return configs;
 }
 
 function mockGetNetworkConfig(networkName: string) {
