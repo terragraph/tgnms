0e55245e6c738e13521bd4b9ea58acdcee0ec756
Cory Modlin <cmodlin@fb.com>
2019-09-12 15:46:28 -0700

    Add mgmtRx.bfTrainingRsp
    Summary:
    Mistakenly added short name mgmtRx.bfTrainingReq instead of bfTrainingRsp.
    
    Would still like to change all
    `tgf.<mac>.name`
    to
    
    metric = `name`
    and nodeMac = `<mac>`
    
    some day ...
    
    Reviewed By: aclave1
    
    Differential Revision: D17251894
    
    fbshipit-source-id: f5f3b817afa431fd4d1e71e6d27b81001338ef68

---
+++ tgnms/fbcnms-projects/tgnms/server/seeders/20190907092700-link-metrics.js
@@ -0,0 +1,15 @@
+
+
+module.exports = {
+  up: (queryInterface, _Sequelize) => {
+    return queryInterface.bulkInsert('link_metric', [{
+      name: 'mgmtrx_bftrainingrsp',
+      key_name: 'mgmtRx.bfTrainingRsp',
+      key_prefix: 'tgf',
+      description: 'Counter for BF Training Rsp slots at Rx',
+    }]);
+  },
+
+  down: (_queryInterface, _Sequelize) => {
+  },
+};
