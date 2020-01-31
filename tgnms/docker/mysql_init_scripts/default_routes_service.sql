06559c86b9bf9392424400ea478cbb6698c7fb61
Shardul Purav <spurav@fb.com>
2020-01-31 15:26:14 -0800

    Move away from raw SQL support for table creation in default routes service
    Summary: Move to Alembic way of dealing with database revisions.
    
    Reviewed By: omikader
    
    Differential Revision: D19603026
    
    fbshipit-source-id: 944baa40afc2c758f6c63ad33e8ccb012e7b4c1c

--- tgnms/docker/mysql_init_scripts/default_routes_service.sql
+++ tgnms/docker/mysql_init_scripts/default_routes_service.sql
@@ -22,38 +22,3 @@ DELIMITER ;
 
 /* create new users only if current user is root */
 call Create_Default_Route();
-
-/* create history table */
-CREATE TABLE IF NOT EXISTS `default_route_history`
-(
-  `id` int(11) AUTO_INCREMENT,
-  `network_name` varchar(100) NOT NULL,
-  `node_name` varchar(255) NOT NULL,
-  `last_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `routes` json NOT NULL,
-  `hop_count` int(11) NOT NULL,
-  `prev_routes_id` int(11),
-  PRIMARY KEY (`id`),
-  KEY `network_name` (`network_name`),
-  KEY `node_name` (`node_name`),
-  KEY `last_updated` (`last_updated`),
-  KEY `prev_routes_id` (`prev_routes_id`),
-  CONSTRAINT `prev_routes_id`
-  FOREIGN KEY (`prev_routes_id`)
-  REFERENCES `default_route_history` (`id`)
-  ON DELETE SET NULL
-);
-
-/* create link CN routes table */
-CREATE TABLE IF NOT EXISTS `link_cn_routes`
-(
-  `id` int(11) AUTO_INCREMENT,
-  `network_name` varchar(100) NOT NULL,
-  `link_name` varchar(255) NOT NULL,
-  `last_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `cn_routes` json NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `network_name` (`network_name`),
-  KEY `link_name` (`link_name`),
-  KEY `last_updated` (`last_updated`)
-);
