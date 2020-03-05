3c21a51d9f9cb8b55ee40e6e990dc69c5294f308
Omar Abdelkader <omikader@fb.com>
2020-03-05 09:56:37 -0800

    Add ansible support for tglib-based microservices
    Summary:
    We want to start adding functional microservices to the our production setup (ansible). I added the default_routes_service and topology_service to the already existing `msa` role (I deleted the old network test because it doesn't work).
    
    The main feature in this diff is that MySQL credentials are generated randomly on the ansible controller machine and used to create the user/database per MSA service. Since the alembic.ini file needs the credentials and cannot load them from the environment, I've templated it and deleted the hardcoded versions in the service directories.
    
    The other change is that I added the MySQL root credentials to the group_vars section since keycloak and the msa roles both need them.
    
    Reviewed By: satyakb
    
    Differential Revision: D20142457
    
    fbshipit-source-id: 942eb05c18c5f5f53725550964c1c4000488ad5d

--- nms_stack/nms_cli/nms_stack/roles/keycloak/templates/scripts/create-keycloak-database.sh
+++ nms_stack/nms_cli/nms_stack/roles/keycloak/templates/scripts/create-keycloak-database.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysql -h $MYSQL_HOST -u $MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "set @keycloak_user='${DB_USER}'; set @keycloak_password='${DB_PASSWORD}'; source /scripts/keycloak.sql;"
+mysql -h$DB_HOST -u$DB_ROOT_USER -p$DB_ROOT_PASSWORD -e "set @keycloak_user='${DB_USER}'; set @keycloak_password='${DB_PASSWORD}'; source /scripts/keycloak.sql;"
--- nms_stack/nms_cli/nms_stack/roles/msa/templates/scripts/msa.sql
+++ nms_stack/nms_cli/nms_stack/roles/msa/templates/scripts/msa.sql
@@ -1,16 +1,21 @@
 -- use dynamic sql so we can pass variables to ddl statements
+set @createDatabaseQuery = CONCAT (
+  'CREATE DATABASE IF NOT EXISTS `', @db_name, '`;'
+);
 set @createUserQuery = CONCAT (
-  'CREATE USER IF NOT EXISTS "', @keycloak_user, '" IDENTIFIED BY "', @keycloak_password, '";'
+  'CREATE USER IF NOT EXISTS "', @db_user, '" IDENTIFIED BY "', @db_password, '";'
 );
 set @grantPrivilegesQuery = CONCAT (
-  'GRANT ALL PRIVILEGES ON keycloak.* TO "', @keycloak_user, '"@"%";'
+  'GRANT ALL PRIVILEGES ON ', @db_name, '.* TO "', @db_user, '"@"%";'
 );
+PREPARE createDatabase from @createDatabaseQuery;
 PREPARE createUser FROM @createUserQuery;
 PREPARE grantPrivileges from @grantPrivilegesQuery;
 
-CREATE DATABASE IF NOT EXISTS `keycloak`;
+EXECUTE createDatabase;
 EXECUTE createUser;
 EXECUTE grantPrivileges;
 FLUSH PRIVILEGES;
+DEALLOCATE PREPARE createDatabase;
 DEALLOCATE PREPARE createUser;
 DEALLOCATE PREPARE grantPrivileges;
