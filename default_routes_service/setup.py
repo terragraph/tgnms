8ef10167ec20a91cf5bc5d05b0f4d4b4262009dd
Omar Abdelkader <omikader@fb.com>
2020-05-08 08:05:30 -0700

    misc(default_routes_servce): update date/version
    Summary: I forgot to update the date/version of this package in D21476093
    
    Reviewed By: spurav
    
    Differential Revision: D21476243
    
    fbshipit-source-id: 2a801dd833a4b5a36865a390b0455409b933b02f

--- default_routes_service/setup.py
+++ default_routes_service/setup.py
@@ -16,7 +16,7 @@ ptr_params = {
 
 setup(
     name="default_routes_service",
-    version="2020.04.08",
+    version="2020.05.08",
     packages=find_packages(exclude=["tests"]),
     install_requires=["aiohttp", "aiomysql", "alembic>=1.3.3,<2.0", "sqlalchemy"],
     extras_require={"ci": ["ptr"], "docs": ["aiohttp-swagger>=1.0.9,<2.0"]},
