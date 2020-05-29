23435b36a5ae26f345ac6ad1200d1a00d65a841b
David Riazati <davidriazati@fb.com>
2020-05-29 13:13:52 -0700

    Delete tgnms-grafana image
    Summary: This is no longer used anywhere due to D21463719, so it can be deleted.
    
    Reviewed By: omikader
    
    Differential Revision: D21652360
    
    fbshipit-source-id: 2f3aad1e824356c88ad983c8510aa8e1e6eb7dda

--- tgnms-grafana/plugins/grafana-beringei-datasource/sync_with_vm.sh
+++
@@ -1,8 +0,0 @@
-#rsync -azP --exclude '.git' --exclude 'node_modules' . root@[2620:10d:c089:e001:4798:5cb6:1dc4:14de]:/home/nms/terragraph-apps/beringei/grafana-beringei-datasource
-rsync -azP --exclude '.git' --exclude 'node_modules' . root@[2620:10d:c089:e001:250:56ff:fea8:76a0]:/var/lib/grafana/plugins/grafana-beringei-datasource
-
-# this command points mysql to the prod server
-# ssh root@2620:10d:c089:e001:4798:5cb6:1dc4:14de /home/nms/www/pointToProdMysql.sh
-#ssh root@2620:10d:c089:e001:4798:5cb6:1dc4:14de systemctl restart nms_prod
-
-
