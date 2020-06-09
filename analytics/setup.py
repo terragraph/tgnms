80d81fc234d5afe6cf081da75fe1db4aa2cb9032
Omar Abdelkader <omikader@fb.com>
2020-06-09 14:20:40 -0700

    misc(tglib): add numpy to testing environment
    Summary:
    Some `tglib` services had `ptr` disabled by default because they use `numpy` and it was difficult to install `numpy` to our testing environment. This diff installs `numpy` to the virtual environment and enables `ptr` for those services.
    
    In order for `numpy` to work, the directory to which the C-extensions are installed must be added to the `$LD_LIBRARY_PATH` on the system. Since sourcing shell scripts doesn't preserve environment variables between steps on sandcastle, I manually exported the variable as part of the `ptr` run step.
    
    The `setup_tests.sh` script will now need to be sourced on the devserver in order to properly export the `$LD_LIBRARY_PATH`.
    
    Reviewed By: cooperlees
    
    Differential Revision: D21952606
    
    fbshipit-source-id: 1a2e7f1f103c27b8952b0ef27f18ddb74879c229

--- analytics/setup.py
+++ analytics/setup.py
@@ -5,8 +5,6 @@ from setuptools import find_packages, setup
 
 
 ptr_params = {
-    # Disable auto running if found - Requires --run-disabled to run
-    "disabled": True,
     "entry_point_module": "analytics/main",
     "test_suite": "tests.base",
     "test_suite_timeout": 300,
@@ -21,6 +19,7 @@ setup(
     version="2020.06.03",
     packages=find_packages(exclude=["tests"]),
     python_requires=">=3.7",
+    install_requires=["numpy>=1.16.4,<2.0"],
     extras_require={"ci": ["ptr"], "docs": ["aiohttp-swagger>=1.0.9,<2.0"]},
     test_suite=ptr_params["test_suite"],
     entry_points={"console_scripts": ["analytics = analytics.main:main"]},
--- scan_service/setup.py
+++ scan_service/setup.py
@@ -5,11 +5,10 @@ from setuptools import find_packages, setup
 
 
 ptr_params = {
-    "disabled": True,
     "entry_point_module": "scan_service/main",
     "test_suite": "tests.base",
     "test_suite_timeout": 600,
-    "required_coverage": {"scan_service/connectivity.py": 80, "TOTAL": 9},
+    "required_coverage": {"scan_service/connectivity.py": 78, "TOTAL": 7},
     "run_flake8": True,
     "run_black": True,
     "run_mypy": True,
@@ -20,7 +19,12 @@ setup(
     version="2020.06.04",
     python_requires=">=3.7",
     packages=find_packages(exclude=["tests"]),
-    install_requires=["aiohttp", "croniter>=0.3.30,<1.0", "sqlalchemy"],
+    install_requires=[
+        "aiohttp",
+        "croniter>=0.3.30,<1.0",
+        "numpy>=1.16.4,<2.0",
+        "sqlalchemy",
+    ],
     extras_require={"ci": ["ptr"], "docs": ["aiohttp-swagger>=1.0.9,<2.0"]},
     test_suite=ptr_params["test_suite"],
     entry_points={"console_scripts": ["scan_service = scan_service.main:main"]},
