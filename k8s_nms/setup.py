3fefd5fc97bfa74b731c8b2bc04a182cc766f7e8
David Riazati <davidriazati@fb.com>
2020-07-16 11:01:21 -0700

    Add build_zip command to generate binary of all resources
    Summary: This adds a `build_zip` command to `setup.py` to create a binary of all the files needed to configure the cluster. It runs `jinja2` to template out files as needed, then adds them all to a single zip file. Since everything is a Kubernetes resource, they can all just be `apply`ed, and Kubernetes will figure out which parts to change.
    
    Reviewed By: aclave1
    
    Differential Revision: D22522189
    
    fbshipit-source-id: 1843acc923b5e03d264ee33056f9a0c483b04ec7

--- k8s_nms/setup.py
+++ k8s_nms/setup.py
@@ -2,9 +2,13 @@
 # Copyright (c) Facebook, Inc.
 
 import distutils
+import glob
+import os
 import pathlib
 import subprocess
+import zipfile
 
+import jinja2
 from setuptools import Command, setup
 
 
@@ -66,9 +70,64 @@ class CloneSubmodulesCommand(Command):
             subprocess.check_call(command)
 
 
+class BuildZip(Command):
+    """Build zip of all Kubernetes resources"""
+
+    description = "Template Kubernetes resources"
+    user_options = []
+
+    def initialize_options(self) -> None:
+        """Set default values for options."""
+        # Distutils complains if this method isn't implemented
+        pass
+
+    def finalize_options(self) -> None:
+        """Post-process options."""
+        # Distutils complains if this method isn't implemented
+        pass
+
+    def run(self) -> None:
+        """Find any ``.yml`` files in a ``templates`` folder and add them to a
+        zip archive."""
+        zip_name = "k8s_resources"
+        with zipfile.ZipFile(f"{zip_name}.zip", "w") as zip_file:
+            for file_name in glob.glob("**/templates/**/*.yml", recursive=True):
+
+                def lookup(type, lookup_file):
+                    path_parts = [os.path.dirname(file_name)]
+                    if lookup_file.startswith(".."):
+                        # The paths for ansible's `lookup` do this automatically
+                        # for some reason, so mirror it here so the paths in the
+                        # template file are the same
+                        path_parts.append("..")
+                    path_parts.append(lookup_file)
+
+                    path = os.path.join(*path_parts)
+                    return open(path, "r").read()
+
+                template = jinja2.Template(open(file_name, "r").read())
+                templated = template.render({"lookup": lookup})
+
+                archive_name = file_name.replace("k8s_nms/ansible/roles/", "")
+                archive_name = archive_name.replace("templates/", "")
+                self.announce(
+                    f"Wrote {archive_name} to {zip_name}.zip", level=distutils.log.INFO
+                )
+                zip_file.writestr(f"{zip_name}/{archive_name}", templated)
+
+        self.announce(
+            f"Finished writing {zip_name}.zip, copy it to the master node and run:",
+            level=distutils.log.INFO,
+        )
+        self.announce(
+            f"unzip {zip_name}.zip && find {zip_name} -type f | sed 's/^/-f /g' | xargs kubectl apply",
+            level=distutils.log.INFO,
+        )
+
+
 setup(
     name="k8s_nms",
-    version="2020.06.30",
+    version="2020.07.16",
     description=("k8s_nms cli"),
     packages=["k8s_nms"],
     package_data={"k8s_nms": ansible_files},
@@ -82,7 +141,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Development Status :: 3 - Alpha",
     ],
-    cmdclass={"clone_submodules": CloneSubmodulesCommand},
+    cmdclass={"clone_submodules": CloneSubmodulesCommand, "build_zip": BuildZip},
     entry_points={"console_scripts": ["k8s_nms = k8s_nms.nms:cli"]},
     python_requires=">=3.7",
     install_requires=["ansible==2.9.9", "click", "setuptools"],
