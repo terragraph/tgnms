b2157da6526d4b47ac8a8cf4aca0b3112a0c6160
David Riazati <davidriazati@fb.com>
2020-07-21 14:05:10 -0700

    Add `nms rage` command again
    Summary: See D22558230 (https://github.com/facebookexternal/terragraph-apps/commit/f72b83d911ee4b2172e6911644a5d90c7b6e5ada), this is mostly the same minus the bugs with reaching into the Python stack with `inspect` that didn't work when the installer was bundled in a pex, plus a test to make sure that commands still work and the logging is catching the stdout/stderr.
    
    Reviewed By: aclave1
    
    Differential Revision: D22607552
    
    fbshipit-source-id: 0b76556658f989134f0a40d55416ddbd25db99c8

---
+++ nms_stack/nms_cli/rage.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# Copyright (c) 2014-present, Facebook, Inc.
+
+import contextlib
+import functools
+import io
+import logging
+import os
+import re
+import shutil
+import sys
+import time
+from typing import List, Optional
+
+
+# Cache the logger
+_logger = None
+
+_invocation_hooks = []
+
+
+def register_invocation_hook(fn):
+    _invocation_hooks.append(fn)
+
+
+def get_next_file_name(base_dir) -> str:
+    """
+    Generate a file name of the current time (plus a number if its conflicting
+    with an existing file)
+    """
+    timestr = time.strftime("%Y.%m.%d-%H.%M.%S")
+    base_path = f"nms_log_{timestr}"
+    path = base_path
+
+    # If for some reason this log already exists, append a number
+    num = 1
+    while os.path.exists(os.path.join(base_dir, path)):
+        path = f"{base_path}_{num}"
+        num += 1
+
+    return os.path.join(base_dir, path)
+
+
+def get_logger(base_dir: Optional[str] = None) -> logging.Logger:
+    """
+    Get a logger that will report to a file inside of base_dir
+    """
+    global _logger
+    if _logger is None:
+        if base_dir is None:
+            raise RuntimeError("Logger was uninitialized, base_dir is required")
+        if not os.path.exists(base_dir):
+            os.mkdir(base_dir)
+
+        _logger = logging.getLogger(__name__)
+        _logger.setLevel(logging.DEBUG)
+
+        f_handler = logging.FileHandler(get_next_file_name(base_dir))
+        f_handler.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
+        f_handler.setFormatter(formatter)
+
+        _logger.addHandler(f_handler)
+    return _logger
+
+
+class TtyLogger(object):
+    """
+    Utility class to override sys.(stdout|stderr).write(). We need this instead of a
+    logging.StreamHandler() since we need to log stdout buffered by line instead
+    of all at once once the program is finished.
+    """
+
+    def __init__(self, logger, terminal):
+        self.terminal = terminal
+        self.logger = logger
+
+    def write(self, message):
+        self.terminal.write(message)
+
+        # Some libraries issue 2 calls to write() for a print, with the second
+        # one being just '\n', so ignore that here
+        if message != "\n" and message != "" and message != "".encode("utf-8"):
+            self.logger.log(logging.INFO, message.rstrip())
+
+    def flush(self):
+        self.terminal.flush()
+
+
+def log_command(base_dir):
+    """
+    Decorator to log a functions arguments and stdout
+    """
+
+    def decorator(fn):
+        @functools.wraps(fn)
+        def wrapper(*args, **kwargs):
+            # initialize logger
+            logger = get_logger(base_dir)
+
+            # Log command line args
+            record_invocation()
+
+            # Log the args passed to the function
+            logger.log(
+                logging.DEBUG,
+                f"Function called with args: {str(args)} and kwargs {str(kwargs)}",
+            )
+
+            # Erase stdout/stderr with out own logging version
+            stdout_logger = TtyLogger(logger, sys.stdout)
+            sys.stdout = stdout_logger
+            stderr_logger = TtyLogger(logger, sys.stderr)
+            sys.stderr = stderr_logger
+
+            # Run the actual function
+            result = fn(*args, **kwargs)
+
+            # Restore the real stdout/stderr
+            sys.stdout = stdout_logger.terminal
+            sys.stderr = stderr_logger.terminal
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+def record_invocation() -> None:
+    """
+    Decorator to log info about Python environment and command line args
+    """
+    get_logger().log(logging.DEBUG, "New command invocation")
+    get_logger().log(logging.DEBUG, sys.executable)
+    get_logger().log(logging.DEBUG, sys.version_info)
+    get_logger().log(logging.DEBUG, f"Invoked with: {sys.argv}")
+
+    for hook in _invocation_hooks:
+        hook(get_logger())
+
+
+def gather_files(base_dir, limit) -> List[str]:
+    """
+    Collect file names for a rage report (sorted by file mtime)
+    """
+    if not os.path.exists(base_dir):
+        return []
+
+    files = [os.path.join(base_dir, path) for path in os.listdir(base_dir)]
+    # TODO: Do we need Windows support? If so switch and use st_ctime
+    # https://stackoverflow.com/questions/6759415/sorting-files-by-date
+    files.sort(key=lambda x: os.stat(os.path.join(base_dir, x)).st_mtime)
+
+    # Don't report all the files, just the NUM_LOGS_TO_REPORT latest
+    return files[-limit:]
+
+
+def clean(base_dir) -> None:
+    print(
+        f"This will remove all the log files in {base_dir}, do you want to continue? (Y/n)",
+        end=" ",
+    )
+    choice = input().lower()
+    if choice == "y":
+        if os.path.exists(base_dir):
+            shutil.rmtree(base_dir)
--- nms_stack/nms_cli/tests/base.py
+++ nms_stack/nms_cli/tests/base.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # Copyright (c) 2014-present, Facebook, Inc.
 
+import io
+import logging
 import os
+import sys
+import tempfile
 import unittest
 
 from click.testing import CliRunner
@@ -15,6 +19,13 @@ def get_config_file(name):
 class FakeExecutor:
     def __init__(*args, **kwargs):
         pass
+
+    def run(self, *args, **kwargs):
+        print(f"stdout: Running fake Ansible {str(args)} and {str(kwargs)}")
+        print(
+            f"stderr: Running fake Ansible {str(args)} and {str(kwargs)}",
+            file=sys.stderr,
+        )
 
     def __getattr__(self, name):
         def empty_fn(*args, **kwargs):
@@ -34,10 +45,45 @@ class NmsTestUtils:
 
         self.assertEqual(0, result.exit_code)
 
-    def check_command(self, command):
+    def check_command(self, command, **kwargs):
         runner = CliRunner()
-        result = runner.invoke(nms.cli, command.split(" "), catch_exceptions=False)
+        result = runner.invoke(
+            nms.cli, command.split(" "), catch_exceptions=False, **kwargs
+        )
         self.check_result(result)
+        return result
+
+
+class FakeLogger:
+    """
+    Context manager to overwrite nms.rage.get_logger with one that doesn't
+    need a folder on the filesystem
+    """
+
+    def __enter__(self, *args, **kwargs):
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+
+        f = io.StringIO()
+        f_handler = logging.StreamHandler(f)
+
+        formatter = logging.Formatter("%(message)s")
+        f_handler.setFormatter(formatter)
+
+        logger.addHandler(f_handler)
+
+        self.old_logger = nms.rage.get_logger
+
+        def get_logger(*args):
+            return logger
+
+        nms.rage.get_logger = get_logger
+
+        return f
+
+    def __exit__(self, *args, **kwargs):
+        pass
+        nms.rage.get_logger = self.old_logger
 
 
 class TestNmsCli(NmsTestUtils, unittest.TestCase):
@@ -64,6 +110,25 @@ class TestNmsCli(NmsTestUtils, unittest.TestCase):
         self.check_command(
             f"upgrade -f {get_config_file('test_group_vars')} -c example_first -i example.com -h fake_host"
         )
+
+    def test_rage(self):
+        with FakeLogger() as f:
+            command = self.check_command(
+                "install -f config.yml -C cert.pem -k key.pem -h fake_host"
+            )
+            self.check_command("rage")
+
+            # Check the command's stdout is expected
+            self.assertEqual(command.output.count("stdout: Running fake Ansible"), 1)
+            self.assertEqual(command.output.count("stderr: Running fake Ansible"), 1)
+
+            rage_log = f.getvalue()
+
+            # Check that the rage logger caught the stdout and added metadata
+            self.assertEqual(rage_log.count("New command invocation"), 1)
+            self.assertEqual(rage_log.count("Function called with args"), 1)
+            self.assertEqual(rage_log.count("stdout: Running fake Ansible"), 1)
+            self.assertEqual(rage_log.count("stderr: Running fake Ansible"), 1)
 
     def test_show_defaults(self):
         self.check_command("show-defaults")
