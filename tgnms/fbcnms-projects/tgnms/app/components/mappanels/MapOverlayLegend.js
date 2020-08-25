485d1ae4f0e1b986ae2f327e0f463350c5956f71
Corey Miner <cjminer@fb.com>
2020-08-25 16:46:38 -0700

    feat(nms): Adding hide ability for legend
    Summary: adding hide and unhide for legend
    
    Reviewed By: aclave1
    
    Differential Revision: D23312065
    
    fbshipit-source-id: 2a0d207472bc6e4475dacbc1f0a1c653612cc960

--- tgnms/fbcnms-projects/tgnms/app/components/mappanels/MapOverlayLegend.js
+++ tgnms/fbcnms-projects/tgnms/app/components/mappanels/MapOverlayLegend.js
@@ -5,13 +5,17 @@
  * @flow
  */
 
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Collapse from '@material-ui/core/Collapse';
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import HealthIndicator from '../common/HealthIndicator';
+import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
+import classNames from 'classnames';
 import {MAP_CONTROL_LOCATIONS} from '../../constants/NetworkConstants';
 import {METRIC_COLOR_RANGE} from '../../constants/LayerConstants';
 import {SpecialNodeOverlayColors} from '../../constants/LayerConstants';
@@ -24,7 +28,13 @@ const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(1.5),
   },
-  title: {paddingLeft: theme.spacing(0.75)},
+  title: {
+    paddingLeft: theme.spacing(0.25),
+    paddingTop: theme.spacing(0.25),
+  },
+  titleWrapper: {
+    marginLeft: -theme.spacing(0.625),
+  },
   resultDivider: {
     margin: `${theme.spacing()}px -${theme.spacing(1.5)}px`,
   },
@@ -33,11 +43,11 @@ const useStyles = makeStyles(theme => ({
     textTransform: 'capitalize',
   },
   siteLegendLabel: {
-    margin: `-${theme.spacing(0.375)}px ${theme.spacing(1.5)}px`,
+    margin: `-${theme.spacing(0.375)}px ${theme.spacing(2)}px`,
     marginLeft: 0,
   },
   linkLegendLabel: {
-    margin: `-${theme.spacing(0.5)}px ${theme.spacing(1.5)}px`,
+    margin: `-${theme.spacing(0.5)}px ${theme.spacing(2)}px`,
     marginLeft: 0,
   },
   linkHealthIndicator: {
@@ -51,11 +61,18 @@ const useStyles = makeStyles(theme => ({
     width: theme.spacing(1.5),
     height: theme.spacing(1.5),
   },
+  rotateIcon: {
+    transform: 'rotate(90deg)',
+  },
+  transition: {
+    transition: 'all 0.3s',
+  },
 }));
 
 export default function MapOverlayLegend() {
   const classes = useStyles();
   const {mapboxRef, overlaysConfig, selectedOverlays} = useMapContext();
+  const [showLegend, setShowLegend] = React.useState(true);
 
   const mapboxControl = React.useMemo(() => {
     const container = document.createElement('div');
@@ -63,6 +80,10 @@ export default function MapOverlayLegend() {
     container.setAttribute('data-testid', 'tg-legend-container');
     return container;
   }, []);
+
+  const toggleLegend = React.useCallback(() => setShowLegend(curr => !curr), [
+    setShowLegend,
+  ]);
 
   React.useEffect(() => {
     mapboxRef?.addControl(
@@ -97,67 +118,84 @@ export default function MapOverlayLegend() {
 
   return ReactDOM.createPortal(
     <Paper className={classes.root} elevation={2}>
-      <Typography className={classes.title} variant="subtitle1">
-        Legend
-      </Typography>
-      <Grid container direction="column">
-        {linkLegend?.map(({labelName, elementColor}) => (
-          <div className={classes.linkLegendLabel}>
-            <Grid item container spacing={1}>
-              <Grid item>
-                <HealthIndicator
-                  color={elementColor}
-                  className={classes.linkHealthIndicator}
-                />
-              </Grid>
-              <Grid item>
-                <Typography className={classes.labelName} variant="body2">
-                  {labelName +
-                    (!labelName.includes('link') && !labelName.match(/\d+/g)
-                      ? ' link'
-                      : '')}
-                </Typography>
-              </Grid>
-            </Grid>
-          </div>
-        ))}
-        <Divider className={classes.resultDivider} />
-        {nodeLegend.map(({labelName, elementColor}) => (
-          <div className={classes.siteLegendLabel}>
-            <Grid item container spacing={1}>
-              <Grid item>
-                <HealthIndicator
-                  color={elementColor}
-                  className={classes.siteHealthIndicator}
-                />
-              </Grid>
-              <Grid item>
-                <Typography className={classes.labelName} variant="body2">
-                  {labelName}
-                </Typography>
-              </Grid>
-            </Grid>
-          </div>
-        ))}
-        <Divider className={classes.resultDivider} />
-        {siteLegend?.map(({labelName, elementColor}) => (
-          <div className={classes.siteLegendLabel}>
-            <Grid item container spacing={1}>
-              <Grid item>
-                <HealthIndicator
-                  color={elementColor}
-                  className={classes.siteHealthIndicator}
-                />
-              </Grid>
-              <Grid item>
-                <Typography className={classes.labelName} variant="body2">
-                  {labelName} site
-                </Typography>
-              </Grid>
-            </Grid>
-          </div>
-        ))}
+      <Grid container className={classes.titleWrapper}>
+        <IconButton
+          size="small"
+          onClick={toggleLegend}
+          data-testid="drawer-toggle-button">
+          <ChevronRightIcon
+            color="secondary"
+            className={classNames(
+              showLegend ? classes.rotateIcon : '',
+              classes.transition,
+            )}
+          />
+        </IconButton>
+        <Typography className={classes.title} variant="subtitle1">
+          Legend
+        </Typography>
       </Grid>
+
+      <Collapse in={showLegend}>
+        <Grid container direction="column">
+          {linkLegend?.map(({labelName, elementColor}) => (
+            <div className={classes.linkLegendLabel}>
+              <Grid item container spacing={1}>
+                <Grid item>
+                  <HealthIndicator
+                    color={elementColor}
+                    className={classes.linkHealthIndicator}
+                  />
+                </Grid>
+                <Grid item>
+                  <Typography className={classes.labelName} variant="body2">
+                    {labelName +
+                      (!labelName.includes('link') && !labelName.match(/\d+/g)
+                        ? ' link'
+                        : '')}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </div>
+          ))}
+          <Divider className={classes.resultDivider} />
+          {nodeLegend.map(({labelName, elementColor}) => (
+            <div className={classes.siteLegendLabel}>
+              <Grid item container spacing={1}>
+                <Grid item>
+                  <HealthIndicator
+                    color={elementColor}
+                    className={classes.siteHealthIndicator}
+                  />
+                </Grid>
+                <Grid item>
+                  <Typography className={classes.labelName} variant="body2">
+                    {labelName}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </div>
+          ))}
+          <Divider className={classes.resultDivider} />
+          {siteLegend?.map(({labelName, elementColor}) => (
+            <div className={classes.siteLegendLabel}>
+              <Grid item container spacing={1}>
+                <Grid item>
+                  <HealthIndicator
+                    color={elementColor}
+                    className={classes.siteHealthIndicator}
+                  />
+                </Grid>
+                <Grid item>
+                  <Typography className={classes.labelName} variant="body2">
+                    {labelName} site
+                  </Typography>
+                </Grid>
+              </Grid>
+            </div>
+          ))}
+        </Grid>
+      </Collapse>
     </Paper>,
     mapboxControl,
   );
