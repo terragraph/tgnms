58c6eb1543b51ce07663b1ef7669fe82ebc660f7
Alex Clavelle <clavelle@fb.com>
2020-10-05 09:16:40 -0700

    BE(tgnms): More docs for annotations
    Summary:
    Adds the remaining swagger docs for the annotation endpoints. I'm still not 100% sold on doing it in the comments unless I can get it really short using `allOf` .
    
    https://pxl.cl/1mCk8
    
    Reviewed By: cjminer505
    
    Differential Revision: D24095995
    
    fbshipit-source-id: 8e2f1440ad4825578a0d727f9d13a2f8667161eb

--- tgnms/fbcnms-projects/tgnms/server/map/routes.js
+++ tgnms/fbcnms-projects/tgnms/server/map/routes.js
@@ -12,6 +12,25 @@ const logger = require('../log')(module);
 
 const router = createApi();
 
+/**
+ * @swagger
+ * paths:
+ *   "/map/annotations/{network}":
+ *     get:
+ *       description: Gets a listing of annotation groups
+ *       tags:
+ *       - Annotations
+ *       produces:
+ *         - application/json
+ *       parameters:
+ *         - $ref: "#/components/parameters/network"
+ *       responses:
+ *         200:
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: "#/components/schemas/AnnotationGroupIdArray"
+ */
 router.get('/annotations/:network', (req, res) => {
   const {network} = req.params;
   return mapService
@@ -49,6 +68,26 @@ router.get('/annotations/:network/:group', (req, res) => {
     .catch(createErrorHandler(res));
 });
 
+/**
+ *  @swagger
+ *  paths:
+ *   "/map/annotations/{network}":
+ *     put:
+ *       description: "Creates or replaces an annotation group and its features.
+ *        This request is idempotent."
+ *       tags:
+ *         - Annotations
+ *       produces:
+ *         - application/json
+ *       parameters:
+ *         - $ref: "#/components/parameters/network"
+ *       responses:
+ *         200:
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: "#/components/schemas/AnnotationGroup"
+ */
 router.put('/annotations/:network', (req, res) => {
   const {network} = req.params;
   if (!req.body?.geojson) {
@@ -61,6 +100,36 @@ router.put('/annotations/:network', (req, res) => {
     .catch(createErrorHandler(res));
 });
 
+/**
+ *  @swagger
+ *  paths:
+ *   "/map/annotations/group/{groupId}":
+ *     put:
+ *       description: "Update mutable properties of annotation group.
+ *        Currently only name is mutable."
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *              type: object
+ *              description: "Mutable properties of annotation group"
+ *              properties:
+ *                name: name
+ *                type: string
+ *       tags:
+ *         - Annotations
+ *       produces:
+ *         - application/json
+ *       parameters:
+ *         - $ref: "#/components/parameters/groupId"
+ *       responses:
+ *         200:
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: "#/components/schemas/AnnotationGroup"
+ */
 router.put('/annotations/group/:groupId', (req, res) => {
   const {groupId} = req.params;
   if (groupId == null) {
@@ -76,6 +145,20 @@ router.put('/annotations/group/:groupId', (req, res) => {
     .catch(createErrorHandler(res));
 });
 
+/**
+ *  @swagger
+ *  paths:
+ *   "/annotations/{network}/{group}":
+ *     delete:
+ *       description: "Delete an annotation group that belongs to a network"
+ *       tags:
+ *         - Annotations
+ *       produces:
+ *         - application/json
+ *       parameters:
+ *         - $ref: "#/components/parameters/network"
+ *         - $ref: "#/components/parameters/group"
+ */
 router.delete('/annotations/:network/:group', (req, res) => {
   const {network, group} = req.params;
   return mapService
@@ -84,6 +167,36 @@ router.delete('/annotations/:network/:group', (req, res) => {
     .catch(createErrorHandler(res));
 });
 
+/**
+ *  @swagger
+ *  paths:
+ *   "/annotations/{network}/{group}/duplicate":
+ *     post:
+ *       description: "Create a copy of an annotation group with a new name"
+ *       tags:
+ *         - Annotations
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *              type: object
+ *              description: "Name of the new annotation group"
+ *              properties:
+ *                name: newName
+ *                type: string
+ *       parameters:
+ *         - $ref: "#/components/parameters/network"
+ *         - $ref: "#/components/parameters/group"
+ *       produces:
+ *         - application/json
+ *       responses:
+ *         200:
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: "#/components/schemas/AnnotationGroup"
+ */
 router.post('/annotations/:network/:groupName/duplicate', (req, res) => {
   const {network, groupName} = req.params;
   const {newName} = req.body;
@@ -96,6 +209,33 @@ router.post('/annotations/:network/:groupName/duplicate', (req, res) => {
     .catch(createErrorHandler(res));
 });
 
+/**
+ *  @swagger
+ *  paths:
+ *   "/annotations/{network}/{group}/{annotationId}":
+ *     put:
+ *       description: "Update one annotation in an annotation group."
+ *       tags:
+ *         - Annotations
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/AnnotationFeature"
+ *       parameters:
+ *         - $ref: "#/components/parameters/network"
+ *         - $ref: "#/components/parameters/group"
+ *         - $ref: "#/components/parameters/annotationId"
+ *       produces:
+ *         - application/json
+ *       responses:
+ *         200:
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: "#/components/schemas/AnnotationFeature"
+ */
 router.put('/annotations/:network/:group/:annotationId', (req, res) => {
   const {network, group, annotationId} = req.params;
   return mapService
@@ -108,6 +248,22 @@ router.put('/annotations/:network/:group/:annotationId', (req, res) => {
     .then(x => res.json(x))
     .catch(createErrorHandler(res));
 });
+
+/**
+ *  @swagger
+ *  paths:
+ *   "/annotations/{network}/{group}/{annotationId}":
+ *     delete:
+ *       description: "Delete one annotation in an annotation group."
+ *       tags:
+ *         - Annotations
+ *       parameters:
+ *         - $ref: "#/components/parameters/network"
+ *         - $ref: "#/components/parameters/group"
+ *         - $ref: "#/components/parameters/annotationId"
+ *       produces:
+ *         - application/json
+ */
 router.delete('/annotations/:network/:group/:annotationId', (req, res) => {
   const {network, group, annotationId} = req.params;
   return mapService
