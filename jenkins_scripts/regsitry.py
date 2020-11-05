714913855eb850910c367723987d2332122ef580
Omar Abdelkader <omikader@fb.com>
2020-11-04 16:53:35 -0800

    feat(ci): generalize image build/push script
    Summary:
    Updates `build_msa_image.py` to be more generic so we can use it for all of our jenkins image building/publishing jobs
    
    There are two subparsers now, `build` and `push`.
    
    Non-MSA service e.g. Kafka
    
    ```
    python3 registry.py build kafka --branch origin/lts-nms-20.10 --context kafka --dir kafka
    python3 registry.py push kafka --username $DOCKER_LOGIN
    ```
    
    Staged build e.g. tglib
    
    ```
    python3 registry.py build tglib --branch origin/master --dir tglib --stage builder
    python3 registry.py build tglib --branch origin/master --dir tglib
    python3 registry.py push tglib --username $DOCKER_LOGIN
    ```
    
    Reviewed By: driazati
    
    Differential Revision: D24716268
    
    fbshipit-source-id: 0093fc497bb4867e4d14193e09566a25857b919f

---
+++ jenkins_scripts/regsitry.py
@@ -0,0 +1,107 @@
+import argparse
+import logging
+import os
+import re
+import subprocess
+from typing import Dict
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def run(cmd: str) -> None:
+    logging.info(f"Running: {cmd}")
+    subprocess.run(cmd, shell=True, check=True)
+
+
+def read(cmd: str) -> str:
+    logging.info(f"Reading: {cmd}")
+    p = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, check=True)
+    return p.stdout.decode("utf-8").strip()
+
+
+def get_commit_info() -> Dict[str, str]:
+    commit_body = read("git log -1 --pretty='%b'")
+    match = re.search(r"Differential Revision: D(\d+)", commit_body)
+    if match is None:
+        diff_num = "<unknown>"
+    else:
+        diff_num = f"D{match.groups()[0]}"
+
+    return {
+        "commit.date": read("git log -1 --pretty='%ci'"),
+        "commit.subject": read("git log -1 --pretty='%s'"),
+        "commit.hash": read("git log -1 --pretty='%h'"),
+        "commit.diff": diff_num,
+    }
+
+
+def build(args: argparse.Namespace) -> None:
+    command = ["docker", "build", "-f", f"{args.dir}/Dockerfile"]
+    if args.branch == "origin/master":
+        if args.stage:
+            command += ["--target", args.stage]
+            release = "dev"
+        else:
+            release = "latest"
+    elif m := re.search(r"origin/(lts-nms-\d{2}\.\d{1,2})", args.branch):
+        if args.stage:
+            raise RuntimeError(f"Cannot build '{args.stage}' stage for {args.branch}")
+        release = m.group(0)
+    else:
+        raise RuntimeError(f"Cannot build for {args.branch}")
+
+    command += ["--tag", f"{args.registry}/{args.name}:{release}"]
+    for label, value in get_commit_info().items():
+        command += ["--label", f'"{label}={value}"']
+
+    command.append(args.context)
+    run(" ".join(command))
+
+
+def push(args: argparse.Namespace) -> None:
+    command = [
+        "echo",
+        os.environ["DOCKER_PASSWORD"],
+        "|",
+        "docker",
+        "login",
+        "-u",
+        args.username,
+        "--password-stdin",
+        f"{args.registry}/v2",
+    ]
+    run(" ".join(command))
+    run(f"docker push {args.registry}/{args.name}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Docker resgistry CLI parser for TG")
+    subparsers = parser.add_subparsers(title="actions")
+    subparsers.required = True
+
+    build_parser = subparsers.add_parser("build")
+    build_parser.add_argument("name", help="docker package name")
+    build_parser.add_argument("--branch", help="git branch that is being built")
+    build_parser.add_argument("--context", help="build context path", default=".")
+    build_parser.add_argument("--dir", help="directory of the Dockerfile", default=".")
+    build_parser.add_argument(
+        "--registry",
+        help="regsitry hostname and port",
+        default="secure.cxl-terragraph.com:443",
+    )
+    build_parser.add_argument("--stage", help="specify a Dockerfile stage")
+    build_parser.set_defaults(func=build)
+
+    push_parser = subparsers.add_parser("push")
+    push_parser.add_argument("name", help="docker package name")
+    push_parser.add_argument(
+        "--registry",
+        help="regsitry hostname and port",
+        default="secure.cxl-terragraph.com:443",
+    )
+    push_parser.add_argument("--username", help="docker registry username")
+    push_parser.set_defaults(func=push)
+
+    args = parser.parse_args()
+    args.func(args)
