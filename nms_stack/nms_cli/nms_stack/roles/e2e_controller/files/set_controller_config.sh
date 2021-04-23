eca40c5678d65b8765790377628c51bef899c36b
Paul McCutcheon <pm@fb.com>
2021-04-23 11:39:30 -0700

    sw upgrade http support via nginx
    Summary:
    - Serve /node_image/<controller name> via nginx.
    - Symlink e2e data folder.
    - Redid our controller config defaults - it did NOT work at all.
    - Set the new e2e flag for node image path.
    
    Reviewed By: aclave1
    
    Differential Revision: D27903587
    
    fbshipit-source-id: 42094e905ea8cc0b1433f91ed6da56c9b11d6173

---
+++ nms_stack/nms_cli/nms_stack/roles/e2e_controller/files/set_controller_config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+CONTROLLER_NAME="$1"
+KEYCLOAK_TOKEN="$2"
+
+API_SERVICE="e2e-${CONTROLLER_NAME}_api_service"
+E2E_CONTROLLER="e2e_controller-${CONTROLLER_NAME}"
+NODE_IMAGE_PATH="/node_image/${CONTROLLER_NAME}/"
+CONTROLLER_CONFIG_FILE=$(mktemp)
+
+CURL_CMD="curl --fail --noproxy '*'"
+if [ -n "$KEYCLOAK_TOKEN" ]; then
+  CURL_CMD="$CURL_CMD -H 'Authorization: Bearer $KEYCLOAK_TOKEN'"
+fi
+
+# fetch existing controller config
+if $CURL_CMD "http://${API_SERVICE}:8080/api/v2/getControllerConfig" > "${CONTROLLER_CONFIG_FILE}" 2>/dev/null; then
+  # set controller config defaults
+  # - zmq_url for connecting e2e_controller/stats_agent
+  # - kafka stats broker endpoint
+  # - node_image HTTP path for SW upgrades
+  if cat "$CONTROLLER_CONFIG_FILE" | jq ".config | fromjson
+      * {\"statsAgentParams\": {\"sources\": {\"controller\": {\"enabled\": true, \"zmq_url\": \"tcp://${E2E_CONTROLLER}:28989\"}}}}
+      * {\"statsAgentParams\": {\"endpointParams\": {\"kafkaParams\": {\"config\": {\"brokerEndpointList\": \"PLAINTEXT://kafka:9092\"}}}}}
+      * {\"flags\": {\"upgrade_image_http_path\" : \"${NODE_IMAGE_PATH}\"}}" | jq '{"config": . | tojson}' | \
+      $CURL_CMD "http://${API_SERVICE}:8080/api/v2/setControllerConfig" --data @- 2>/dev/null | jq '.success'; then
+    exit 0
+  fi
+fi
+exit 1
