f0d74272297de91f4eca36ed61f03a595f7cb818
Alex Clavelle <clavelle@fb.com>
2021-05-06 12:34:46 -0700

    fix(tgnms): Mapbox navigation control repeatedly appended
    Summary:
    D28204878 (https://github.com/terragraph/tgnms/commit/873074f40a822063b09dbf4a8db80756408c2a7e) was the cause
    https://pxl.cl/1HPcL
    
    Reviewed By: cjminer505
    
    Differential Revision: D28263169
    
    fbshipit-source-id: 835c7a63cf54d6951f4684a867cb139602b1dc7a

--- tgnms/fbcnms-projects/tgnms/app/views/map/mapControls/mapboxNavigation/MapboxNavigationControl.js
+++ tgnms/fbcnms-projects/tgnms/app/views/map/mapControls/mapboxNavigation/MapboxNavigationControl.js
@@ -12,11 +12,11 @@ import {useMapContext} from '@fbcnms/tg-nms/app/contexts/MapContext';
 
 export default function MapboxNavigationControl() {
   const {mapboxRef} = useMapContext();
-  const navControl = new mapboxgl.NavigationControl({});
 
   React.useEffect(() => {
+    const navControl = new mapboxgl.NavigationControl({});
     mapboxRef?.addControl(navControl, MAP_CONTROL_LOCATIONS.TOP_LEFT);
-  }, [mapboxRef, navControl]);
+  }, [mapboxRef]);
 
   return null;
 }
