ee6c1ea1d3f022325dcf96cb2399963cf0e85068
Joy Dang <joydang@fb.com>
2021-09-02 16:30:35 -0700

    fix(nano): reorganize file structure in crash log analysis service
    Summary: reorganization of the crash log analysis service directory structure to better fit the structure of a NANO service.
    
    Reviewed By: silver23arrow
    
    Differential Revision: D30438926
    
    fbshipit-source-id: eda2dae5631c31f0899e98c63f9557bc59c7d340

--- crashlog_analysis_service/tests/test_crash_analyzer.py
+++ crashlog_analysis_service/tests/test_crash_analyzer.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python3
-
-"""
-Copyright (c) Facebook, Inc. and its affiliates.
-All rights reserved.
-"""
-
-"""
-Test class for CrashAnalyzer
-"""
+# Copyright 2004-present Facebook. All Rights Reserved.
 
 import unittest
 import sys
 
-sys.path.append("../utils/")
+sys.path.append("..")
 try:
-  from crash_analyzer import CrashAnalyzer
-  from crash_details import CrashDetails
+    from crashlog_analysis_service.utils.crash_analyzer import CrashAnalyzer
+    from crashlog_analysis_service.utils.crash_details import CrashDetails
 except BaseException:
-  raise
+    raise
 
 from typing import AnyStr, Dict, List, Optional, Tuple
+
+"""
+Test class for CrashAnalyzer
+"""
 
 # stack trace log file path
 STACK_TRACE_FILE_PATH = "./test_logs/e2e_minion-stack.4095.Tue_Mar_30_17_52_00_PDT_2021"
@@ -40,136 +36,141 @@ BAD_STACK_TRACE_FILE_PATH = "./test_logs/bad_stack_trace_e2e_minion.log"
 # bad file path name
 BAD_FILE_PATH = "./test_logs/this_file_does_not_exist.log"
 
+
 class CrashAnalyzerTestCase(unittest.TestCase):
+    def setUp(self):
+        file_paths: List[str] = [
+            STACK_TRACE_FILE_PATH,
+            VNET_LOG_FILE_PATH,
+            FW_ERROR_LOG_FILE_PATH,
+            EMPTY_LOG_FILE_PATH,
+            BAD_STACK_TRACE_FILE_PATH,
+        ]
+        self.file_path_to_lines: Dict[str, List[str]] = {}
+        for file_path in file_paths:
+            crash_file = open(file_path, "r")
+            self.file_path_to_lines[file_path] = crash_file.readlines()
+            crash_file.close()
+        crash_file = open(STACK_TRACE_FILE_PATH, "r")
+        self.stack_trace_lines: List[str] = crash_file.readlines()
+        crash_file.close()
 
-  def setUp(self):
-    file_paths: List[str] = [
-      STACK_TRACE_FILE_PATH,
-      VNET_LOG_FILE_PATH,
-      FW_ERROR_LOG_FILE_PATH,
-      EMPTY_LOG_FILE_PATH,
-      BAD_STACK_TRACE_FILE_PATH
-    ]
-    self.file_path_to_lines: Dict[str, List[str]] = {}
-    for file_path in file_paths:
-      crash_file = open(file_path, 'r')
-      self.file_path_to_lines[file_path] = crash_file.readlines()
-      crash_file.close()
-    crash_file = open(STACK_TRACE_FILE_PATH, 'r')
-    self.stack_trace_lines: List[str] = crash_file.readlines()
-    crash_file.close()
+    def test_extract_time(self):
+        test_cases: Dict[str, Optional[str]] = {
+            "extra_text_12:42:41.123456abcd": "12:42:41.123456",
+            "extra_text_12:4a:41.123456abcd": None,
+            "extra_text_12:42:41:123456abcd": None,
+            "212:42:41.123456abcd": "12:42:41.123456",
+        }
+        crashAnalyzer = CrashAnalyzer()
+        for test_str in test_cases:
+            exp_res = test_cases[test_str]
+            test_res = crashAnalyzer.extract_time(test_str)
+            self.assertEqual(test_res, exp_res)
 
-  def test_extract_time(self):
-    test_cases: Dict[str, Optional[str]] = {
-      "extra_text_12:42:41.123456abcd": "12:42:41.123456",
-      "extra_text_12:4a:41.123456abcd": None,
-      "extra_text_12:42:41:123456abcd": None,
-      "212:42:41.123456abcd": "12:42:41.123456"
-    }
-    crashAnalyzer = CrashAnalyzer()
-    for test_str in test_cases:
-      exp_res = test_cases[test_str]
-      test_res = crashAnalyzer.extract_time(test_str)
-      self.assertEqual(test_res, exp_res)
+    def test_extract_time_from_log_path(self):
+        test_cases: Dict[str, Optional[str]] = {
+            "openr-stack.30500.Fri_Jun_11_16_31_50_UTC_2021": "16:31:50",
+            "openr-stack.30500.Fri_Jun_11_16/31/50/UTC_2021": "16:31:50",
+            "openr-stack.30500.Fri_Jun_11_16_31_50_20_123_2021": None,
+        }
+        crashAnalyzer = CrashAnalyzer()
+        for test_str in test_cases:
+            exp_res = test_cases[test_str]
+            test_res = crashAnalyzer.extract_time_from_log_path(test_str)
+            self.assertEqual(test_res, exp_res)
 
-  def test_extract_time_from_log_path(self):
-    test_cases: Dict[str, Optional[str]] = {
-      "openr-stack.30500.Fri_Jun_11_16_31_50_UTC_2021": "16:31:50",
-      "openr-stack.30500.Fri_Jun_11_16/31/50/UTC_2021": "16:31:50",
-      "openr-stack.30500.Fri_Jun_11_16_31_50_20_123_2021": None
-    }
-    crashAnalyzer = CrashAnalyzer()
-    for test_str in test_cases:
-      exp_res = test_cases[test_str]
-      test_res = crashAnalyzer.extract_time_from_log_path(test_str)
-      self.assertEqual(test_res, exp_res)
+    def test_extract_function_name(self):
+        test_cases: Dict[str, Optional[str]] = {
+            "#0  0x0000ffffa89f9560 in raise () from /lib/libc.so.6": "raise",
+            "#4  0x0000ffffa9e07f8c in ?? () from /usr/lib/libopenrlib.so.1": "??",
+            "#4  0x0000ffffa9e07f8c in ?? ()": "??",
+            "4  0x0000ffffa9e07f8c in ?? ()": None,
+            "#4  0x0000ffffa9e07f8c in ??()": None,
+            "#4  0x0000ffffa9e07f8c! in ?? ()": None,
+        }
+        crashAnalyzer = CrashAnalyzer()
+        for test_str in test_cases:
+            exp_res = test_cases[test_str]
+            test_res = crashAnalyzer.extract_function_name(test_str)
+            self.assertEqual(test_res, exp_res)
 
-  def test_extract_function_name(self):
-    test_cases: Dict[str, Optional[str]] = {
-      "#0  0x0000ffffa89f9560 in raise () from /lib/libc.so.6": "raise",
-      "#4  0x0000ffffa9e07f8c in ?? () from /usr/lib/libopenrlib.so.1": "??",
-      "#4  0x0000ffffa9e07f8c in ?? ()": "??",
-      "4  0x0000ffffa9e07f8c in ?? ()": None,
-      "#4  0x0000ffffa9e07f8c in ??()": None,
-      "#4  0x0000ffffa9e07f8c! in ?? ()": None
-    }
-    crashAnalyzer = CrashAnalyzer()
-    for test_str in test_cases:
-      exp_res = test_cases[test_str]
-      test_res = crashAnalyzer.extract_function_name(test_str)
-      self.assertEqual(test_res, exp_res)
+    def test_extract_current_thread(self):
+        test_cases: Dict[str, Optional[str]] = {
+            "Current thread is 123": "123",
+            "Current thread is 1": "1",
+            "Current thread is abc": None,
+            "": None,
+        }
+        crashAnalyzer = CrashAnalyzer()
+        for test_str in test_cases:
+            exp_res = test_cases[test_str]
+            test_res = crashAnalyzer.extract_current_thread(test_str)
+            self.assertEqual(test_res, exp_res)
 
-  def test_extract_current_thread(self):
-    test_cases: Dict[str, Optional[str]] = {
-      "Current thread is 123": "123",
-      "Current thread is 1": "1",
-      "Current thread is abc": None,
-      "": None
-    }
-    crashAnalyzer = CrashAnalyzer()
-    for test_str in test_cases:
-      exp_res = test_cases[test_str]
-      test_res = crashAnalyzer.extract_current_thread(test_str)
-      self.assertEqual(test_res, exp_res)
+    def test_extract_thread_stack_trace(self):
+        test_cases: Dict[str, int] = {"1": 34, "15": 21, "20": 0, "abc": 0}
+        crashAnalyzer = CrashAnalyzer()
+        for test_thread in test_cases:
+            exp_res = test_cases[test_thread]
+            test_res = crashAnalyzer._extract_thread_stack_trace(
+                test_thread, self.stack_trace_lines
+            )
+            self.assertEqual(len([] if test_res == None else test_res), exp_res)
 
-  def test_extract_thread_stack_trace(self):
-    test_cases: Dict[str, int] = {
-      "1": 34,
-      "15": 21,
-      "20": 0,
-      "abc": 0
-    }
-    crashAnalyzer = CrashAnalyzer()
-    for test_thread in test_cases:
-      exp_res = test_cases[test_thread]
-      test_res = crashAnalyzer._extract_thread_stack_trace(test_thread, self.stack_trace_lines)
-      self.assertEqual(len([] if test_res == None else test_res), exp_res)
+    def test_parse_stack_trace_log(self):
+        # filepath -> len(test_res.affected_lines)
+        test_cases: Dict[str, int] = {
+            EMPTY_LOG_FILE_PATH: 0,
+            BAD_STACK_TRACE_FILE_PATH: 24,
+            STACK_TRACE_FILE_PATH: 34,
+        }
+        crashAnalyzer = CrashAnalyzer()
+        for test_file in test_cases:
+            exp_res = test_cases[test_file]
+            lines = []
+            with open(test_file, "r") as crash_file:
+                lines = crash_file.readlines()
+            test_res = crashAnalyzer.parse_stack_trace_log(lines, "", "", None)
+            self.assertEqual(
+                len([] if test_res == None else test_res.affected_lines), exp_res
+            )
 
-  def test_parse_stack_trace_log(self):
-    # filepath -> len(test_res.affected_lines)
-    test_cases: Dict[str, int] = {
-      EMPTY_LOG_FILE_PATH: 0,
-      BAD_STACK_TRACE_FILE_PATH: 24,
-      STACK_TRACE_FILE_PATH: 34
-    }
-    crashAnalyzer = CrashAnalyzer()
-    for test_file in test_cases:
-      exp_res = test_cases[test_file]
-      lines = []
-      with open(test_file, 'r') as crash_file:
-        lines = crash_file.readlines()
-      test_res = crashAnalyzer.parse_stack_trace_log(lines, "", "", None)
-      self.assertEqual(len([] if test_res == None else test_res.affected_lines), exp_res)
+    def test_find_error_msg(self):
+        # (file path, error msg) -> (len(test_res), len(test_res[0].affected_lines))
+        test_cases: Dict[(str, str), (int, int)] = {
+            (EMPTY_LOG_FILE_PATH, "dummy msg"): (0, 0),
+            (FW_ERROR_LOG_FILE_PATH, "(Firmware error) detected, assert codes"): (
+                1,
+                29,
+            ),
+        }
+        crashAnalyzer = CrashAnalyzer()
+        for test_file, error_msg in test_cases:
+            exp_res_first, exp_res_second = test_cases[(test_file, error_msg)]
+            lines = []
+            with open(test_file, "r") as crash_file:
+                lines = crash_file.readlines()
+            test_res = crashAnalyzer.find_error_msg(error_msg, lines, "", "", None)
+            self.assertEqual(len(test_res), exp_res_first)
+            if len(test_res) != 0:
+                self.assertEqual(len(test_res[0].affected_lines), exp_res_second)
 
-  def test_find_error_msg(self):
-    # (file path, error msg) -> (len(test_res), len(test_res[0].affected_lines))
-    test_cases: Dict[(str, str), (int, int)] = {
-      (EMPTY_LOG_FILE_PATH, "dummy msg"): (0, 0),
-      (FW_ERROR_LOG_FILE_PATH, "(Firmware error) detected, assert codes"): (1, 29),
-    }
-    crashAnalyzer = CrashAnalyzer()
-    for test_file, error_msg in test_cases:
-      exp_res_first, exp_res_second = test_cases[(test_file, error_msg)]
-      lines = []
-      with open(test_file, 'r') as crash_file:
-        lines = crash_file.readlines()
-      test_res = crashAnalyzer.find_error_msg(error_msg, lines, "", "", None)
-      self.assertEqual(len(test_res), exp_res_first)
-      if len(test_res) != 0:
-        self.assertEqual(len(test_res[0].affected_lines), exp_res_second)
+    def test_run_error_parsers(self):
+        # file path -> len(test_res)
+        test_cases: Dict[str, int] = {
+            EMPTY_LOG_FILE_PATH: 0,
+            STACK_TRACE_FILE_PATH: 1,
+            FW_ERROR_LOG_FILE_PATH: 0,
+        }
+        crashAnalyzer = CrashAnalyzer()
+        for test_file in test_cases:
+            exp_res = test_cases[test_file]
+            test_res = crashAnalyzer.run_error_parsers(
+                test_file, self.file_path_to_lines[test_file], "", ""
+            )
+            self.assertEqual(len(test_res), exp_res)
 
-  def test_run_error_parsers(self):
-    # file path -> len(test_res)
-    test_cases: Dict[str, int] = {
-      EMPTY_LOG_FILE_PATH: 0,
-      STACK_TRACE_FILE_PATH: 1,
-      FW_ERROR_LOG_FILE_PATH: 0,
-    }
-    crashAnalyzer = CrashAnalyzer()
-    for test_file in test_cases:
-      exp_res = test_cases[test_file]
-      test_res = crashAnalyzer.run_error_parsers(test_file, self.file_path_to_lines[test_file], "", "")
-      self.assertEqual(len(test_res), exp_res)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- crashlog_analysis_service/tests/test_crash_key.py
+++ crashlog_analysis_service/tests/test_crash_key.py
@@ -1,58 +1,60 @@
 #!/usr/bin/env python3
-
-"""
-Copyright (c) Facebook, Inc. and its affiliates.
-All rights reserved.
-"""
-
-"""
-Test class for CrashKey
-"""
+# Copyright 2004-present Facebook. All Rights Reserved.
 
 import unittest
 import sys
 
 sys.path.append("../")
 try:
-  import crash_analysis_runner
-
-  from crash_details import CrashDetails
-  from crash_key import CrashKey
+    from crashlog_analysis_service.utils.crash_details import CrashDetails
+    from crashlog_analysis_service.utils.crash_key import CrashKey
 except BaseException:
-  raise
+    raise
 
 from typing import AnyStr, Dict, List, Optional, Tuple
 
+"""
+Test class for CrashKey
+"""
+
+
 class CrashKeyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.key1: CrashKey = CrashKey(crash_type="SIGSEGV", application="vnet")
+        self.key2: CrashKey = CrashKey(crash_type="SIGSEGV", application="vnet")
+        self.key3: CrashKey = CrashKey(crash_type="SIGABRT", application="e2e_minion")
+        self.key4: CrashKey = CrashKey()
 
-  def setUp(self):
-    self.key1: CrashKey = CrashKey(crash_type="SIGSEGV", application="vnet")
-    self.key2: CrashKey = CrashKey(crash_type="SIGSEGV", application="vnet")
-    self.key3: CrashKey = CrashKey(crash_type="SIGABRT", application="e2e_minion")
-    self.key4: CrashKey = CrashKey()
+    def test_init(self):
+        self.assertEqual(self.key1.crash_type, "SIGSEGV")
+        self.assertEqual(self.key1.application, "vnet")
+        self.assertEqual(self.key1.crash_time, None)
+        self.assertEqual(self.key1.node_id, None)
 
-  def test_init(self):
-    self.assertEqual(self.key1.crash_type, "SIGSEGV")
-    self.assertEqual(self.key1.application, "vnet")
-    self.assertEqual(self.key1.crash_time, None)
-    self.assertEqual(self.key1.node_id, None)
+    def test_eq(self):
+        self.assertTrue(self.key1.__eq__(self.key2))
+        self.assertFalse(self.key1.__eq__(self.key3))
 
-  def test_eq(self):
-    self.assertTrue(self.key1.__eq__(self.key2))
-    self.assertFalse(self.key1.__eq__(self.key3))
+    def test_str(self):
+        self.assertEqual(
+            self.key1.__str__(), "[ -- Crash type: SIGSEGV -- Application: vnet -- ]"
+        )
+        self.assertEqual(
+            self.key3.__str__(),
+            "[ -- Crash type: SIGABRT -- Application: e2e_minion -- ]",
+        )
+        self.assertEqual(self.key4.__str__(), "[ -- ]")
 
-  def test_str(self):
-    self.assertEqual(self.key1.__str__(), "[ -- Crash type: SIGSEGV -- Application: vnet -- ]")
-    self.assertEqual(self.key3.__str__(), "[ -- Crash type: SIGABRT -- Application: e2e_minion -- ]")
-    self.assertEqual(self.key4.__str__(), "[ -- ]")
+    def test_create_key_from_base(self):
+        crash_detail: CrashDetails = CrashDetails(
+            "SIGSEGV", "11:22:33.444", "", "vnet", "function", []
+        )
+        crash_key: CrashKey = self.key1.create_key_from_base(crash_detail)
+        self.assertEqual(crash_key.application, "vnet")
+        self.assertEqual(crash_key.crash_type, "SIGSEGV")
+        self.assertEqual(crash_key.crash_time, None)
+        self.assertEqual(crash_key.node_id, None)
 
-  def test_create_key_from_base(self):
-    crash_detail: CrashDetails = CrashDetails("SIGSEGV", "11:22:33.444", "", "vnet", "function" , [])
-    crash_key: CrashKey = self.key1.create_key_from_base(crash_detail)
-    self.assertEqual(crash_key.application, "vnet")
-    self.assertEqual(crash_key.crash_type, "SIGSEGV")
-    self.assertEqual(crash_key.crash_time, None)
-    self.assertEqual(crash_key.node_id, None)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
