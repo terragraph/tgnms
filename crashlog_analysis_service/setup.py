e5f31e0171cbc96a96f30adb3215d90c89e72b00
Joy Dang <joydang@fb.com>
2021-09-02 16:55:46 -0700

    feat(nano): add base files for crash log analysis service
    Summary: Add base and initialization files for the NANO service.
    
    Reviewed By: omikader
    
    Differential Revision: D30439289
    
    fbshipit-source-id: d5aac78e7054c0111eb2984f96cd7a000ddceb01

---
+++ crashlog_analysis_service/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# Copyright 2004-present Facebook. All Rights Reserved.
+
+from setuptools import find_packages, setup
+
+
+ptr_params = {
+    "entry_point_module": "crashlog_analysis_service/main",
+    "test_suite": "tests.base",
+    "test_suite_timeout": 300,
+    "run_flake8": True,
+    "run_black": True,
+    "run_mypy": True,
+}
+
+setup(
+    name="crashlog_analysis_service",
+    version="2021.08.17",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "aiohttp",
+        "aiomysql",
+        "alembic>=1.3.3,<2.0",
+        "sqlalchemy",
+        "elasticsearch",
+    ],
+    extras_require={"ci": ["ptr"], "docs": ["aiohttp-swagger>=1.0.9,<2.0"]},
+    test_suite=ptr_params["test_suite"],
+    entry_points={
+        "console_scripts": [
+            "crashlog_analysis_service = crashlog_analysis_service.main:main"
+        ]
+    },
+)
