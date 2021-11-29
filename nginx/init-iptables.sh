9a73c84c44060a846f963cc2fad1b31b44eb92dd
Alex Clavelle <clavelle@fb.com>
2021-11-29 14:14:30 -0800

    fix(tgnms): fix TOS headers
    Summary:
    # Issue
    D30280417 (https://github.com/terragraph/tgnms/commit/e2a9015bc898b96496d121ecd04be6640ef3ca66) was supposed to tag traffic from e2e->nodes with the TOS ip header so that nodes would know to prioritize it. This is done by setting some iptables rules on nginx. These iptables rules were incorrectly applying to *incoming* traffic, node->e2e when in reality it should be outgoing traffic. from e2e->node.
    
    # Fix
    Use the --sport argument instead of --dstport
    
    Reviewed By: elludraon
    
    Differential Revision: D32703887
    
    fbshipit-source-id: 936bf9117aa29b704d67a94f760af4260d648d36

--- nginx/init-iptables.sh
+++ nginx/init-iptables.sh
@@ -4,8 +4,8 @@ echo "Setting TOS headers"
 for p in $PORTS
 do
     echo "Setting TOS for port $p"
-    iptables -A OUTPUT -t mangle -p tcp --dport $p -j TOS --set-tos 0x88
-    ip6tables -A OUTPUT -t mangle -p tcp --dport $p -j TOS --set-tos 0x88
+    iptables -A OUTPUT -t mangle -p tcp --sport $p -j TOS --set-tos 0x88
+    ip6tables -A OUTPUT -t mangle -p tcp --sport $p -j TOS --set-tos 0x88
 done
 
 iptables -t mangle -L OUTPUT
