9672a6b4adacd11a3820addc1c2ee93ea0b30ab8
Tommy Huynh <tommylehuynh@fb.com>
2021-12-17 15:01:17 -0800

    Back out "Installer commit image D32959128, D32969196, D32998245, D33189756"
    Summary: Original Phabricator Diffs: D32959128 (https://github.com/terragraph/tgnms/commit/6b8ed6e094c510e43f3adeab39cfa43fd456cc3a), D32969196 (https://github.com/terragraph/tgnms/commit/8e213795626046e89a34932b77e00406dd6ee0f1), D32998245 (https://github.com/terragraph/tgnms/commit/6ace1fcb6269e356981679d34502565b937b7b6c), D33189756 (https://github.com/terragraph/tgnms/commit/45ff44521e6e5e29afc1e5ed4bc377c1837702b4)
    
    Reviewed By: aclave1
    
    Differential Revision: D33198197
    
    fbshipit-source-id: 8a68d6a0c8c1a95a429a8c33250cf433cfabc421

--- jenkins_scripts/registry.py
+++ jenkins_scripts/registry.py
@@ -6,8 +6,6 @@ import re
 import subprocess
 from typing import Dict
 
-from shared import get_next_tag, read, get_release
-
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -17,13 +15,14 @@ def run(cmd: str) -> None:
     subprocess.run(cmd, shell=True, check=True)
 
 
-def _read(cmd: str) -> str:
+def read(cmd: str) -> str:
     logging.info(f"Reading: {cmd}")
-    return read(cmd)
+    p = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, check=True)
+    return p.stdout.decode("utf-8").strip()
 
 
 def get_commit_info() -> Dict[str, str]:
-    commit_body = _read("git log -1 --pretty='%b'")
+    commit_body = read("git log -1 --pretty='%b'")
     match = re.search(r"Differential Revision: D(\d+)", commit_body)
     if match is None:
         diff_num = "<unknown>"
@@ -31,29 +30,29 @@ def get_commit_info() -> Dict[str, str]:
         diff_num = f"D{match.groups()[0]}"
 
     return {
-        "commit.date": _read("git log -1 --pretty='%ci'"),
-        "commit.subject": _read("git log -1 --pretty='%s'"),
-        "commit.hash": _read("git log -1 --pretty='%h'"),
+        "commit.date": read("git log -1 --pretty='%ci'"),
+        "commit.subject": read("git log -1 --pretty='%s'"),
+        "commit.hash": read("git log -1 --pretty='%h'"),
         "commit.diff": diff_num,
     }
 
 
 def build(args: argparse.Namespace) -> None:
     command = ["docker", "build", "-f", f"{args.dir}/Dockerfile"]
-    release = get_release(args.branch, args.stage)
-    if re.search(r"origin/(main|master)", args.branch) and args.stage:
-        command += ["--target", args.stage]
-
-    # Tag the image with the release version
-    if args.tag:
-        logging.info(f"Tagging image with custom tag: {args.tag}")
-        command += ["--tag", f"{args.registry}/{args.name}:{args.tag}"]
+    if re.search(r"origin/(main|master)", args.branch):
+        if args.stage:
+            command += ["--target", args.stage]
+            release = "dev"
+        else:
+            release = "latest"
+    elif m := re.search(r"origin/releases/(lts-nms-\d{2}\.\d{1,2})", args.branch):
+        if args.stage:
+            raise RuntimeError(f"Cannot build '{args.stage}' stage for {args.branch}")
+        release = m.group(1)
     else:
-        version_tag = get_next_tag(release, printer=logging.info)
-        logging.info(f"Tagging image with tag: {version_tag}")
-        command += ["--tag", f"{args.registry}/{args.name}:{release}"]
-        command += ["--tag", f"{args.registry}/{args.name}:{version_tag}"]
+        raise RuntimeError(f"Cannot build for {args.branch}")
 
+    command += ["--tag", f"{args.registry}/{args.name}:{release}"]
     command += ["--build-arg", f'"TAG={release}"']
     for arg in args.build_arg or []:
         command += ["--build-arg", f'"{arg}"']
@@ -77,11 +76,7 @@ def push(args: argparse.Namespace) -> None:
         f"{args.registry}/v2",
     ]
     run(" ".join(command))
-    if args.tag:
-        push_cmd = f"docker push {args.registry}/{args.name}:{args.tag}"
-    else:
-        push_cmd = f"docker push --all-tags {args.registry}/{args.name}"
-    run(push_cmd)
+    run(f"docker push {args.registry}/{args.name}")
 
 
 if __name__ == "__main__":
@@ -101,10 +96,6 @@ if __name__ == "__main__":
         default="secure.cxl-terragraph.com:443",
     )
     build_parser.add_argument("--stage", help="specify a Dockerfile stage")
-    build_parser.add_argument(
-        "--tag",
-        help="tag this image with this tag only",
-    )
     build_parser.set_defaults(func=build)
 
     push_parser = subparsers.add_parser("push")
@@ -115,10 +106,6 @@ if __name__ == "__main__":
         default="secure.cxl-terragraph.com:443",
     )
     push_parser.add_argument("--username", help="docker registry username")
-    push_parser.add_argument(
-        "--tag",
-        help="specific docker image tag to push, default is all tags in repository",
-    )
     push_parser.set_defaults(func=push)
 
     args = parser.parse_args()
--- nms_stack/nms_cli/nms.py
+++ nms_stack/nms_cli/nms.py
@@ -17,39 +17,19 @@ from pygments import highlight
 from pygments.formatters import TerminalFormatter
 from pygments.lexers import YamlLexer
 
-VERSION_FILE = "__version__"
+
 INSTALL_PLAYBOOK = "install.yml"
 UNINSTALL_PLAYBOOK = "uninstall.yml"
 VALIDATE_PLAYBOOK = "validate.yml"
 
-IMAGE_TEMPLATE = "{image}:{version}"
 RAGE_DIR = os.path.join(os.path.expanduser("~"), ".nms_logs")
 
 executor = ansible_executor.ansible_executor
 
 
-def get_nms_version():
-    version_file = os.path.join(os.path.dirname(__file__), VERSION_FILE)
-    if os.path.exists(version_file):
-        with open(version_file, "r") as f:
-            version = f.read().strip()
-            f.close()
-        if version:
-            return version
-    return None
-
-
-def get_version(installer_opts):
-    return (
-        installer_opts.image_version
-        if installer_opts.image_version
-        else get_nms_version()
-    )
-
-
 def record_version(logger):
     try:
-        version = get_nms_version()
+        version = pkg_resources.get_distribution("nms").version
     except Exception as e:
         version = f"[nms not packged, version unknown]\n{e}"
     logger.log(logging.DEBUG, f"nms version: {version}")
@@ -91,17 +71,10 @@ def gather_hosts(ctx, installer_opts, variables):
     return hosts
 
 
-def generate_variables(ctx, installer_opts, version="latest"):
+def generate_variables(ctx, installer_opts):
     config_file = installer_opts.config_file
     variables = load_variables(config_file)
-
-    certs_config = gather_certs(ctx, installer_opts, variables)
-    variables.update(certs_config)
-
-    # We must overwrite the images before generating common configs.
-    images_configs = generate_image_configs(variables, version)
-    variables.update(images_configs)
-
+    gather_certs(ctx, installer_opts, variables)
     generated_config = generate_common_configs(variables)
     variables.update(generated_config)
     return variables
@@ -118,44 +91,18 @@ def generate_common_configs(variables):
     return generated_config
 
 
-def _gather_image_keys(variables):
-    keys = []
-    for key in variables.keys():
-        if re.match("^[a-z0-9_]+_image", key):
-            keys.append(key)
-    return keys
-
-
 def gather_docker_images(variables):
     # Enumerate docker images to be pulled
     docker_images = []
-    for key in _gather_image_keys(variables):
-        image = variables[key]
-        docker_images.append(image)
+    for key in variables.keys():
+        if re.match("^[a-z0-9_]+_image", key):
+            image = variables[key]
+            docker_images.append(image)
     return docker_images
-
-
-def generate_image_configs(variables, version):
-    new_images = {}
-    for key in _gather_image_keys(variables):
-        image = variables[key]
-        # Ex:
-        #   Has version: secure.cxl-terragraph.com:443/tg-alarms:v21.12.01
-        #   No version: secure.cxl-terragraph.com:443/tg-alarms
-        path = image.split("/", 1)  # split path into repo name and image name
-        if len(path) > 1:
-            image_name = path[1]
-            if len(image_name.split(":")) == 1:
-                # if a version was NOT specified
-                new_images[key] = IMAGE_TEMPLATE.format(image=image, version=version)
-            else:
-                new_images[key] = image
-    return new_images
 
 
 def gather_certs(ctx, installer_opts, variables):
     # Get and verify TLS cert/key.
-    certs = {}
     ssl_key_file = installer_opts.ssl_key_file or variables["ssl_key_file"]
     ssl_cert_file = installer_opts.ssl_cert_file or variables["ssl_cert_file"]
     cert_options = [ssl_key_file, ssl_cert_file]
@@ -166,9 +113,8 @@ def gather_certs(ctx, installer_opts, variables):
             err=True,
         )
         ctx.exit(3)
-    certs["ssl_cert_file"] = os.path.abspath(ssl_cert_file)
-    certs["ssl_key_file"] = os.path.abspath(ssl_key_file)
-    return certs
+    variables["ssl_cert_file"] = os.path.abspath(ssl_cert_file)
+    variables["ssl_key_file"] = os.path.abspath(ssl_key_file)
 
 
 def prepare_ansible(ctx, installer_opts, variables):
@@ -215,10 +161,6 @@ common_options = {
         "-p", "--password", help="SSH/sudo password for setup bootstrap", is_flag=True
     ),
     "verbose": click.option("-v", "--verbose", count=True, default=0),
-    "image-version": click.option(
-        "--image-version",
-        help="Which image version to install. Default is latest version.",
-    ),
 }
 
 
@@ -242,7 +184,6 @@ class InstallerOpts(object):
         self.tags = kwargs.pop("tags", None)
         self.verbose = kwargs.pop("verbose", None)
         self.password = kwargs.pop("password", None)
-        self.image_version = kwargs.pop("image_version", None)
         self.other_options = kwargs
 
 
@@ -254,7 +195,6 @@ def add_installer_opts(common_opts=common_options.keys()):
             return ctx.invoke(f, InstallerOpts(**kwargs), *args)
 
         return update_wrapper(wrapper, f)
-
     return fn_wrapper
 
 
@@ -267,7 +207,7 @@ def cli(ctx, version, short):
     if version or short:
         longver = "Terragraph NMS cli utility version: {}"
         try:
-            verstr = get_nms_version()
+            verstr = pkg_resources.get_distribution("nms").version
         except Exception as e:
             click.echo("Cannot find package version, is this in a package?")
             click.echo(f"{e}")
@@ -422,9 +362,7 @@ def install(ctx, installer_opts):
     password = None
     if installer_opts.password:
         password = click.prompt("SSH/sudo password", hide_input=True, default=None)
-
-    version = get_version(installer_opts)
-    variables = generate_variables(ctx, installer_opts, version)
+    variables = generate_variables(ctx, installer_opts)
     hosts = gather_hosts(ctx, installer_opts, variables)
     a = prepare_ansible(ctx, installer_opts, variables)
     sys.exit(
@@ -467,9 +405,7 @@ def install(ctx, installer_opts):
     is_flag=True,
     help="[Dangerous] Ignore any errors and continue uninstalling",
 )
-@add_installer_opts(
-    common_opts=["config-file", "host", "tags", "password", "verbose", "image-version"]
-)
+@add_installer_opts(common_opts=["config-file", "host", "tags", "password", "verbose"])
 @click.pass_context
 @rage.log_command(RAGE_DIR)
 def uninstall(ctx, installer_opts):
@@ -485,18 +421,15 @@ def uninstall(ctx, installer_opts):
 
     a = executor(installer_opts.tags, installer_opts.verbose)
 
-    version = get_version(installer_opts)
-    variables = generate_variables(ctx, installer_opts, version)
-    variables.update(
-        {
-            "skip_backup": other_options.get("skip_backup"),
-            "delete_data": other_options.get("delete_data"),
-            "backup_file": os.path.abspath(other_options.get("backup_file")),
-            "remove_docker": other_options.get("remove_docker"),
-            "remove_gluster": other_options.get("remove_gluster"),
-            "force": other_options.get("force"),
-        }
-    )
+    variables = generate_variables(ctx, installer_opts)
+    variables.update({
+        "skip_backup": other_options.get('skip_backup'),
+        "delete_data": other_options.get('delete_data'),
+        "backup_file": os.path.abspath(other_options.get('backup_file')),
+        "remove_docker": other_options.get('remove_docker'),
+        "remove_gluster": other_options.get('remove_gluster'),
+        "force": other_options.get('force'),
+    })
     hosts = generate_host_groups(installer_opts.host)
     sys.exit(
         a.run(
@@ -518,8 +451,7 @@ def validate(ctx, installer_opts):
     if installer_opts.password:
         password = click.prompt("SSH/sudo password", hide_input=True, default=None)
     config_file = installer_opts.config_file
-    version = get_version(installer_opts)
-    variables = generate_variables(ctx, installer_opts, version)
+    variables = generate_variables(ctx, installer_opts)
     hosts = gather_hosts(ctx, installer_opts, variables)
     a = prepare_ansible(ctx, installer_opts, variables)
     results = a.run(
