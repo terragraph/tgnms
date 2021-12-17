8f77970503f96f8c7cd745bb803ea0bd73f9807d
tommylehuynh <tommylehuynh@devvm1824.atn0.facebook.com>
2021-12-17 08:20:28 -0800

    DEV github_release debugging

--- jenkins_scripts/github_release.py
+++ jenkins_scripts/github_release.py
@@ -96,90 +96,91 @@ def release(ctx, tag, asset, name, draft, force):
         click.echo(click.style("Error: no release tag", fg="red"))
         return exit(1)
 
-    github_user = os.environ.get("GITHUB_USER")
-    github_access_token = os.environ.get("GITHUB_ACCESS_TOKEN")
-    if github_access_token is None:
-        raise RuntimeError("GITHUB_ACCESS_TOKEN environment variable is required")
-    if github_user is None:
-        raise RuntimeError("GITHUB_USER environment variable is required")
+    click.echo('Inside release')
+    # github_user = os.environ.get("GITHUB_USER")
+    # github_access_token = os.environ.get("GITHUB_ACCESS_TOKEN")
+    # if github_access_token is None:
+    #     raise RuntimeError("GITHUB_ACCESS_TOKEN environment variable is required")
+    # if github_user is None:
+    #     raise RuntimeError("GITHUB_USER environment variable is required")
 
-    headers = {
-        "Accept": "application/vnd.github.v3+json",
-        "Authorization": f"token {github_access_token}",
-    }
-    click.echo(f"Searching for release with tag: {tag}")
-    r = requests.get(f"{API_URL}/releases/tags/{tag}")
-    if r.ok:
-        release_id = r.json()["id"]
-        click.echo(f"Found release: {release_id} with tag {tag}")
-    else:
-        click.echo(f"Creating release for tag: {tag}")
-        r = requests.post(
-            f"{API_URL}/releases",
-            headers=headers,
-            json={
-                "name": tag,
-                "tag_name": tag,
-                "draft": draft,
-                # Mark main-branch releases as prerelease and lts as main releases
-                "prerelease": False if "lts" in tag else True,
-                "body": "NMS installer",
-            },
-        )
-        r.raise_for_status()
+    # headers = {
+    #     "Accept": "application/vnd.github.v3+json",
+    #     "Authorization": f"token {github_access_token}",
+    # }
+    # click.echo(f"Searching for release with tag: {tag}")
+    # r = requests.get(f"{API_URL}/releases/tags/{tag}")
+    # if r.ok:
+    #     release_id = r.json()["id"]
+    #     click.echo(f"Found release: {release_id} with tag {tag}")
+    # else:
+    #     click.echo(f"Creating release for tag: {tag}")
+    #     r = requests.post(
+    #         f"{API_URL}/releases",
+    #         headers=headers,
+    #         json={
+    #             "name": tag,
+    #             "tag_name": tag,
+    #             "draft": draft,
+    #             # Mark main-branch releases as prerelease and lts as main releases
+    #             "prerelease": False if "lts" in tag else True,
+    #             "body": "NMS installer",
+    #         },
+    #     )
+    #     r.raise_for_status()
 
-        release_json = r.json()
-        release_id = release_json["id"]
-        click.echo(f"Created new release: {release_id}")
+    #     release_json = r.json()
+    #     release_id = release_json["id"]
+    #     click.echo(f"Created new release: {release_id}")
 
-    click.echo("Searching for existing release assets")
-    filename = os.path.basename(asset)
-    existing_assets = requests.get(
-        f"{API_URL}/releases/{release_id}/assets",
-        headers=headers,
-    ).json()
+    # click.echo("Searching for existing release assets")
+    # filename = os.path.basename(asset)
+    # existing_assets = requests.get(
+    #     f"{API_URL}/releases/{release_id}/assets",
+    #     headers=headers,
+    # ).json()
 
-    # print out the existing assets
-    click.echo(f"Found {len(existing_assets)} assets")
-    for a in existing_assets:
-        click.echo(f'  {a.get("name")}')
+    # # print out the existing assets
+    # click.echo(f"Found {len(existing_assets)} assets")
+    # for a in existing_assets:
+    #     click.echo(f'  {a.get("name")}')
 
-    existing_asset = next((x for x in existing_assets if x.get("name") == name), None)
-    # A release asset must be deleted before it can be overwritten
-    if existing_asset != None:
-        n = existing_asset.get("name")
-        click.echo(f"Release asset already exists: {n}")
-        if force == False:
-            click.echo(
-                click.style(
-                    f"ERROR: Refusing to overwrite existing asset: {n} without the force param (-f / --force)",
-                    fg="red",
-                )
-            )
-            return exit(1)
-        else:
-            click.echo(f" Deleting existing release asset: {n}")
-            requests.delete(
-                f"{API_URL}/releases/assets/{existing_asset.get('id')}",
-                headers=headers,
-            )
+    # existing_asset = next((x for x in existing_assets if x.get("name") == name), None)
+    # # A release asset must be deleted before it can be overwritten
+    # if existing_asset != None:
+    #     n = existing_asset.get("name")
+    #     click.echo(f"Release asset already exists: {n}")
+    #     if force == False:
+    #         click.echo(
+    #             click.style(
+    #                 f"ERROR: Refusing to overwrite existing asset: {n} without the force param (-f / --force)",
+    #                 fg="red",
+    #             )
+    #         )
+    #         return exit(1)
+    #     else:
+    #         click.echo(f" Deleting existing release asset: {n}")
+    #         requests.delete(
+    #             f"{API_URL}/releases/assets/{existing_asset.get('id')}",
+    #             headers=headers,
+    #         )
 
-    # Upload the asset file
-    upload_headers = headers.copy()
-    upload_headers.update({"Content-Type": "application/octet-stream"})
-    click.echo(f"Uploading asset: {name} to release {tag}")
-    with open(asset, "rb") as file:
-        url = f"https://uploads.github.com/repos/terragraph/tgnms/releases/{release_id}/assets?name={name}&label={name}"
-        r = requests.post(
-            url,
-            headers=upload_headers,
-            data=file,
-        )
-        if r.ok:
-            click.echo(f"Successfully uploaded asset: {filename} to release: {tag}")
-            click.echo(
-                f"Visit: {r.json().get('browser_download_url')} to download the asset"
-            )
+    # # Upload the asset file
+    # upload_headers = headers.copy()
+    # upload_headers.update({"Content-Type": "application/octet-stream"})
+    # click.echo(f"Uploading asset: {name} to release {tag}")
+    # with open(asset, "rb") as file:
+    #     url = f"https://uploads.github.com/repos/terragraph/tgnms/releases/{release_id}/assets?name={name}&label={name}"
+    #     r = requests.post(
+    #         url,
+    #         headers=upload_headers,
+    #         data=file,
+    #     )
+    #     if r.ok:
+    #         click.echo(f"Successfully uploaded asset: {filename} to release: {tag}")
+    #         click.echo(
+    #             f"Visit: {r.json().get('browser_download_url')} to download the asset"
+    #         )
 
 
 def run(cmd: str) -> None:
