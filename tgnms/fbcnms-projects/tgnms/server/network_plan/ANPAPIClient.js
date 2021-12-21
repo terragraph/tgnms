97b4c8d234fc97a57c979c2e3f0f7dd047ab98b2
Alex Clavelle <clavelle@fb.com>
2021-12-21 09:28:14 -0800

    feat(tgnms): Log planner api requests/responses to a file
    Summary:
    For fine-grained planner api debugging, pass the env-var `PLANNER_REQUEST_LOGFILE=./anp-logs.txt` to write every request/response as JSON to this file.
    
    *Analyze the logfile with the jq cli utility:*
    ```
    # show the url of each request
    jq -r '.request.url' < anplog.txt
    # show response headers
    jq -r '.response.headers["x-fb-trace-id"]' < anplog.txt
    # delete things from the json file
    jq 'del(.request.data)' < anplog.txt > processed.txt
    ```
    
    Reviewed By: tommyhuynh
    
    Differential Revision: D33238705
    
    fbshipit-source-id: a4a0bf8e90ac50a3bee2e106b986860fdb6b16a3

--- tgnms/fbcnms-projects/tgnms/server/network_plan/ANPAPIClient.js
+++ tgnms/fbcnms-projects/tgnms/server/network_plan/ANPAPIClient.js
@@ -5,9 +5,11 @@
  * @format
  */
 
+import * as fs from 'fs';
 import axios from 'axios';
 import {DEFAULT_FILE_UPLOAD_CHUNK_SIZE} from '@fbcnms/tg-nms/shared/dto/FacebookGraph';
 import {Readable} from 'stream';
+import {isPlainObject, pick} from 'lodash';
 import {stringify} from 'querystring';
 import {trimEnd} from 'lodash';
 import type {$AxiosXHR} from 'axios';
@@ -394,7 +396,6 @@ export default class ANPAPIClient {
       headers,
       method,
     });
-    logAxiosResponse(response);
     return response.data;
   }
 
@@ -429,14 +430,16 @@ export default class ANPAPIClient {
       data: data,
       responseType: responseType ?? 'json',
     };
+
     try {
       const response = await axios<TReq | void, TRes>(config);
+      logAxios(response);
       return response;
     } catch (err) {
       if (err.response) {
-        logAxiosResponse(err.response);
+        logAxios(err.response);
       } else {
-        console.dir(err);
+        console.error(err);
       }
       throw err;
     }
@@ -495,10 +498,24 @@ export default class ANPAPIClient {
   };
 }
 
-function logAxiosResponse<T, R>(xhr: $AxiosXHR<T, R>) {
-  const {data, headers, status, statusText, config} = xhr;
-  console.dir({
-    response: {data, headers, status, statusText},
-    request: config,
-  });
+function logAxios<T, R>(xhr: $AxiosXHR<T, R>) {
+  const logfile = process.env.PLANNER_REQUEST_LOGFILE;
+  if (typeof logfile === 'string') {
+    try {
+      const {data, headers, status, statusText, config} = xhr;
+      const log = {
+        request: pick(config, ['headers', 'method', 'url']),
+        response: {
+          data: isPlainObject(data) ? data : '[Omitted]',
+          headers,
+          status,
+          statusText,
+        },
+      };
+      fs.appendFileSync(logfile, JSON.stringify(log, null, 2) + '\n');
+    } catch (err) {
+      console.error(xhr);
+      console.error(err);
+    }
+  }
 }
