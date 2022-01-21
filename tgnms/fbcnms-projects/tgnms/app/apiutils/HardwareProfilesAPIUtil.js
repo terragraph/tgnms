23d1d5ffbdb070234889fbea9d96b1e0167cff87
Alex Clavelle <clavelle@fb.com>
2022-01-21 10:19:04 -0800

    fix(tgnms): Fix invalid hwprofiles import
    Summary:
    HardwareProfilesAPIUtil was importing the default hardware profiles from outside the docker root, meaning that the file was not available when building the image.
    
    The original purpose of this import is to provide the default hardware profiles in situations such as when upgrading the image but not upgrading via installer
    
    Reviewed By: tommyhuynh
    
    Differential Revision: D33685469
    
    fbshipit-source-id: 847d784e052fc9a55c108cf0490edc33c93bdf38

--- tgnms/fbcnms-projects/tgnms/app/apiutils/HardwareProfilesAPIUtil.js
+++ tgnms/fbcnms-projects/tgnms/app/apiutils/HardwareProfilesAPIUtil.js
@@ -14,21 +14,6 @@ export async function getSchema(): Promise<Object> {
 }
 
 export async function getAllProfiles(): Promise<HardwareProfiles> {
-  try {
-    const response = await axios.get('/hwprofile');
-    return response.data;
-  } catch (err) {
-    /**
-     * This adds the default profile to the app as a separate chunk. If the
-     * hwprofiles directory is not properly configured, the NMS will still have
-     * some defaults in the UI.
-     */
-    const defaultProfileJSON = await import(
-      // $FlowIgnore importing JSON from outside of flow root
-      '../../../../../nms_stack/nms_cli/nms_stack/roles/hwprofiles/files/profiles/default.json'
-    );
-    return {
-      default: defaultProfileJSON,
-    };
-  }
+  const response = await axios.get('/hwprofile');
+  return response.data;
 }
