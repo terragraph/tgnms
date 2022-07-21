d4d3f0393881920173038c8864e4b278e10db432
Jeffrey Han <jeffreyhan@fb.com>
2022-07-21 09:58:45 -0700

    fix(tgnms): update license headers for 'query_service'
    Summary: 'query_service' sources incorrectly used BSD+Patents headers, but this subproject is actually licensed under straight BSD.
    
    Reviewed By: deltice
    
    Differential Revision: D38018868
    
    fbshipit-source-id: a9598e8ed42c84614be8e513a2fc5127c5fce188

--- query_service/src/KafkaStatsService.cpp
+++ query_service/src/KafkaStatsService.cpp
@@ -1,10 +1,9 @@
 /**
- * Copyright (c) 2016-present, Facebook, Inc.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * LICENSE file in the root directory of this source tree.
  */
 
 #include "KafkaStatsService.h"
--- query_service/src/tests/TestMain.cpp
+++ query_service/src/tests/TestMain.cpp
@@ -1,12 +1,11 @@
-/*
- *  Copyright (c) 2017, Facebook, Inc.
- *  All rights reserved.
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
  *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
- *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
  */
+
 #include <gtest/gtest.h>
 
 #include <folly/init/Init.h>
